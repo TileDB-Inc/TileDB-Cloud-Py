bc8625b04d788f66566d699fbe73a93824bbcdd3
Seth Shelnutt <seth@tiledb.io>
2019-10-25 16:39:11 -0400

    Remove udf sub class, switch apply to take subarray instead of slicing

--- tiledb/cloud/cloudarray.py
+++ tiledb/cloud/cloudarray.py
@@ -2,7 +2,10 @@ import urllib
 from . import rest_api
 from . import config
 from . import client
-from . import udf
+
+import cloudpickle
+import base64
+import sys
 
 
 def split_uri(uri):
@@ -16,6 +19,40 @@ def split_uri(uri):
   if not parsed.scheme == "tiledb":
     raise Exception("Incorrect array uri, must be in tiledb:// scheme")
   return parsed.netloc, parsed.path[1:]
+
+
+def build_dimension_coordinate(domain_type, val):
+  """
+  Build a dimension coordinate model
+  :param domain_type: tiledb dimension type
+  :param val: value to put as coordinate
+  :return: rest_api.models.DimensionCoordinate: model
+  """
+
+  if domain_type == "int8":
+    return rest_api.models.DimensionCoordinate(int8=int(val))
+  elif domain_type == "uint8":
+    return rest_api.models.DimensionCoordinate(uint8=int(val))
+  elif domain_type == "int16":
+    return rest_api.models.DimensionCoordinate(int16=int(val))
+  elif domain_type == "uint16":
+    return rest_api.models.DimensionCoordinate(uint16=int(val))
+  elif domain_type == "int32":
+    return rest_api.models.DimensionCoordinate(int32=int(val))
+  elif domain_type == "uint32":
+    return rest_api.models.DimensionCoordinate(uint32=int(val))
+  elif domain_type == "int64":
+    return rest_api.models.DimensionCoordinate(int64=int(val))
+  elif domain_type == "uint64":
+    return rest_api.models.DimensionCoordinate(uint64=int(val))
+  elif domain_type == "float32":
+    return rest_api.models.DimensionCoordinate(float32=float(val))
+  elif domain_type == "float64":
+    return rest_api.models.DimensionCoordinate(float64=float(val))
+  else:
+    raise Exception("Unsupported dimension type {} for apply udf".format(domain_type))
+
+  return None
 
 
 class CloudArray(object):
@@ -97,7 +134,7 @@ class CloudArray(object):
 
     return api_instance.deregister_array(namespace=namespace, array=array_name)
 
-  def apply(self, func):
+  def apply(self, func, subarray, attrs=None, layout='R'):
     """
     Apply a user defined function to a udf
 
@@ -107,10 +144,28 @@ class CloudArray(object):
     ...   return numpy.median(df["a"])
     >>> # Open the array then run the UDF
     >>> with tiledb.SparseArray("tiledb://user/myarray", ctx=tiledb.cloud.ctx()) as A:
-    ...   A.apply(median)[0:4]
+    ...   A.apply(median, [(0,4)], attrs=["a", "b", "c"])
     2.0
 
     :param func: user function to run
     :return: results of applied udf
     """
-    return udf.UDF(self, func)
+
+    (namespace, array_name) = split_uri(self.uri)
+    api_instance = client.get_udf_api()
+
+    pickledUDF = cloudpickle.dumps(func)
+    pickledUDF = base64.b64encode(pickledUDF).decode('ascii')
+
+    ranges=[]
+    idx = 0
+    for dim in self.schema.domain:
+      start = build_dimension_coordinate(dim.dtype, subarray[idx][0])
+      end = build_dimension_coordinate(dim.dtype, subarray[idx][1])
+      ranges.append(rest_api.models.UDFSubarrayRange(dimension_id=idx, range_start=start, range_end=end))
+      idx = idx + 1
+
+    ranges = rest_api.models.UDFSubarray(layout=layout, ranges=ranges)
+
+    res = api_instance.submit_udf(namespace=namespace, array=array_name, udf=rest_api.models.UDF(type=rest_api.models.UDFType.PYTHON, _exec=pickledUDF, subarray=ranges, version="{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)))
+    return cloudpickle.loads(res)
