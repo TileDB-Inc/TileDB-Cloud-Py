4f6196b2fc83ae2916e2db889b66243543c32d9e
Seth Shelnutt <seth@tiledb.io>
2019-11-05 13:30:44 -0500

    Add output schema support to sql exec

--- tiledb/cloud/sql.py
+++ tiledb/cloud/sql.py
@@ -9,7 +9,7 @@ from .rest_api import rest
 
 import tiledb
 
-def exec(query, output_uri=None, namespace=None, task_name=None):
+def exec(query, output_uri=None, output_schema=None, namespace=None, task_name=None, output_array_name=None):
   """
   Run a sql query
   :param str query: query to run
@@ -31,6 +31,17 @@ def exec(query, output_uri=None, namespace=None, task_name=None):
     namespace = config.user.username
 
   api_instance = client.get_sql_api()
+
+
+  # If the user passes an output schema create the output array
+  if output_schema is not None and output_uri is not None:
+    # Create the (empty) output array in the service
+    tiledb.Array.create(output_uri, output_schema)
+
+    # If the user wishes to set a specific array name for the newly registered output array let's update the details
+    if output_array_name is not None:
+      array_api = client.get_array_api()
+      array_api.update_array_metadata(namespace=namespace, output_uri=output_uri, array_metadata=rest_api.models.ArrayMetadataUpdate(name=output_array_name))
 
   try:
 
@@ -68,30 +79,17 @@ def exec_and_fetch(query, output_uri, output_schema=None, namespace=None, task_n
 
     namespace = config.user.username
 
-  tiledb_output_uri = output_uri
-
-  # If the user passes an output schema create the output array
-  if output_schema is not None and output_uri is not None:
-    tiledb_output_uri = "tiledb://{}/{}".format(namespace, output_uri)
-    # Create the (empty) output array in the service
-    tiledb.Array.create(tiledb_output_uri, output_schema)
-
-    # If the user wishes to set a specific array name for the newly registered output array let's update the details
-    if output_array_name is not None:
-      array_api = client.get_array_api()
-      array_api.update_array_metadata(namespace=namespace, output_uri=output_uri, array_metadata=rest_api.models.ArrayMetadataUpdate(name=output_array_name))
-
   # Execute the sql query
   try:
-    exec(query=query, output_uri=tiledb_output_uri, namespace=namespace, task_name=task_name)
+    exec(query=query, output_uri=output_uri, output_schema=output_schema, namespace=namespace, task_name=task_name, output_array_name=output_array_name)
 
     # Fetch output schema to check if its sparse or dense
-    schema = tiledb.ArraySchema.load(tiledb_output_uri, ctx=client.Ctx())
+    schema = tiledb.ArraySchema.load(output_uri, ctx=client.Ctx())
 
     if schema.sparse:
-      return tiledb.SparseArray(tiledb_output_uri, ctx=client.Ctx())
+      return tiledb.SparseArray(output_uri, ctx=client.Ctx())
 
-    return tiledb.DenseArray(tiledb_output_uri, ctx=client.Ctx())
+    return tiledb.DenseArray(output_uri, ctx=client.Ctx())
 
   except GenApiException as exc:
     raise tiledb_cloud_error.check_exc(exc) from None
