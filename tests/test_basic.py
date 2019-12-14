1309192e961f310f252ce450351c671d35e4673b
Isaiah Norton <isaiah@tiledb.io>
2019-12-14 15:33:29 -0500

    Use unittest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,18 +1,20 @@
 import tiledb, tiledb.cloud
 import os
+import sys, os, platform, unittest
 
 tiledb.cloud.login(token=os.environ["TILEDB_CLOUD_HELPER_VAR"])
 
 
-def test_quickstart():
-    with tiledb.open(
-        "tiledb://TileDB-Inc/quickstart_dense", ctx=tiledb.cloud.Ctx()
-    ) as A:
-        print("quickstart_dense:")
-        print(A[:])
+class BasicTests(unittest.TestCase):
+    def test_quickstart(self):
+        with tiledb.open(
+            "tiledb://TileDB-Inc/quickstart_dense", ctx=tiledb.cloud.Ctx()
+        ) as A:
+            print("quickstart_dense:")
+            print(A[:])
 
-    with tiledb.open(
-        "tiledb://TileDB-Inc/quickstart_sparse", ctx=tiledb.cloud.Ctx()
-    ) as A:
-        print("quickstart_sparse:")
-        print(A[:])
+        with tiledb.open(
+            "tiledb://TileDB-Inc/quickstart_sparse", ctx=tiledb.cloud.Ctx()
+        ) as A:
+            print("quickstart_sparse:")
+            print(A[:])
