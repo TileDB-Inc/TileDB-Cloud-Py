a8a8fbd24930e60273955df0f536f3de3140569a
Seth Shelnutt <seth@tiledb.io>
2020-02-14 14:57:51 -0500

    Add new functions for registering, updating and listing udfs

--- tiledb/cloud/udf.py
+++ tiledb/cloud/udf.py
@@ -41,7 +41,7 @@ def exec_async(
         namespace = config.user.username
 
     if not callable(func):
-        raise TypeError("First argument to `apply` must be callable!")
+        raise TypeError("First argument to `exec` must be callable!")
 
     pickledUDF = cloudpickle.dumps(func, protocol=tiledb_cloud_protocol)
     pickledUDF = base64.b64encode(pickledUDF).decode("ascii")
@@ -103,3 +103,210 @@ def exec(
         image_name=image_name,
         http_compressor=http_compressor,
     ).get()
+
+
+def register_udf(func, name, namespace=None, image_name=None, type=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :param type: type of udf, generic or single_array
+  :return:
+  """
+
+    try:
+        api_instance = client.client.udf_api
+
+        # If the namespace is not set, we will default to the user's namespace
+        if namespace is None:
+            # Fetch the client profile for username if it is not already cached
+            if config.user is None:
+                config.user = client.user_profile()
+
+            namespace = config.user.username
+
+        if not callable(func):
+            raise TypeError("First argument to `exec` must be callable!")
+
+        pickledUDF = cloudpickle.dumps(func, protocol=tiledb_cloud_protocol)
+        pickledUDF = base64.b64encode(pickledUDF).decode("ascii")
+
+        udf = rest_api.models.UDFRegistration(
+            name=name,
+            language=rest_api.models.UDFLanguage.PYTHON,
+            version="{}.{}.{}".format(
+                sys.version_info.major, sys.version_info.minor, sys.version_info.micro,
+            ),
+            image_name=image_name,
+            type=type,
+            _exec=pickledUDF,
+            exec_raw=None,
+        )
+
+        api_instance.register_udf(namespace=namespace, name=name, udf=udf)
+
+    except GenApiException as exc:
+        raise tiledb_cloud_error.check_exc(exc) from None
+
+
+def register_generic_udf(func, name, namespace=None, image_name=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :return:
+  """
+    return register_udf(
+        func=func,
+        name=name,
+        namespace=namespace,
+        image_name=image_name,
+        type=rest_api.models.UDFType.GENERIC,
+    )
+
+
+def register_single_array_udf(func, name, namespace=None, image_name=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :return:
+  """
+    return register_udf(
+        func=func,
+        name=name,
+        namespace=namespace,
+        image_name=image_name,
+        type=rest_api.models.UDFType.SINGLE_ARRAY,
+    )
+
+
+def update_udf(func, name, namespace=None, image_name=None, type=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :param type: type of udf, generic or single_array
+  :return:
+  """
+
+    try:
+        api_instance = client.client.udf_api
+
+        # If the namespace is not set, we will default to the user's namespace
+        if namespace is None:
+            # Fetch the client profile for username if it is not already cached
+            if config.user is None:
+                config.user = client.user_profile()
+
+            namespace = config.user.username
+
+        if not callable(func):
+            raise TypeError("First argument to `exec` must be callable!")
+
+        pickledUDF = cloudpickle.dumps(func, protocol=tiledb_cloud_protocol)
+        pickledUDF = base64.b64encode(pickledUDF).decode("ascii")
+
+        udf = rest_api.models.UDFRegistration(
+            name=name,
+            language=rest_api.models.UDFLanguage.PYTHON,
+            version="{}.{}.{}".format(
+                sys.version_info.major, sys.version_info.minor, sys.version_info.micro,
+            ),
+            image_name=image_name,
+            type=type,
+            _exec=pickledUDF,
+            exec_raw=None,
+        )
+
+        api_instance.updated_registered_udf(namespace=namespace, name=name, udf=udf)
+
+    except GenApiException as exc:
+        raise tiledb_cloud_error.check_exc(exc) from None
+
+
+def update_generic_udf(func, name, namespace=None, image_name=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :return:
+  """
+    return update_udf(
+        func=func,
+        name=name,
+        namespace=namespace,
+        image_name=image_name,
+        type=rest_api.models.UDFType.GENERIC,
+    )
+
+
+def update_single_array_udf(func, name, namespace=None, image_name=None):
+    """
+
+  :param func: function to register
+  :param name: name of udf to register
+  :param namespace: namespace to register in
+  :param image_name: optional image name
+  :return:
+  """
+    return update_udf(
+        func=func,
+        name=name,
+        namespace=namespace,
+        image_name=image_name,
+        type=rest_api.models.UDFType.SINGLE_ARRAY,
+    )
+
+
+def info(name=None):
+    """
+  Fetch info on a registered udf
+  :param name: name of udf in "namespace/name" format
+  :return: registered udf details
+  """
+    try:
+        api_instance = client.client.udf_api
+        (namespace, udf_name) = name.split("/")
+
+        if not (
+            namespace is not None
+            and udf_name is not None
+            and namespace != ""
+            and udf_name != ""
+        ):
+            # If the namespace is not set, we will default to the user's namespace
+            # Fetch the client profile for username if it is not already cached
+            if config.user is None:
+                config.user = client.user_profile()
+
+            namespace = config.user.username
+
+        return api_instance.get_udf(namespace=namespace, name=name)
+    except GenApiException as exc:
+        raise tiledb_cloud_error.check_exc(exc) from None
+
+
+def list_registered_udfs(namespace=None, search=None):
+    """
+  Fetch all registered udf user has access to
+  :param namespace: namespace to filter to
+  :param search: string search for udfs
+  :return: registered udf details
+  """
+    try:
+        api_instance = client.client.udf_api
+
+        return api_instance.get_ud_fs(namespace=namespace, search=search)
+    except GenApiException as exc:
+        raise tiledb_cloud_error.check_exc(exc) from None
