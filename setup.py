2d46437ae52ed4ac9260278d1368059d5a0f0d67
Seth Shelnutt <Shelnutt2@gmail.com>
2020-05-27 19:01:52 -0400

    Update tiledb-plot-widget for supporting multi-parent dags

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ REQUIRES = [
     "plotly>= 4.0.0",
     "networkx>= 2.0.0",
     "pydot",
-    "tiledb-plot-widget",
+    "tiledb-plot-widget>=0.1.7",
 ]
 
 # NOTE: we cannot use an __init__.py file in the tiledb/ directory, because it is supplied
