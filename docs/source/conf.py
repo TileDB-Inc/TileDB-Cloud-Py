4b0fa5cb079ef98883e858f80eebc4eeb220aab9
Seth Shelnutt <Shelnutt2@gmail.com>
2020-09-17 07:16:31 -0400

    Formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,24 +13,24 @@
 import os
 import sys
 
-#sys.path.insert(0, os.path.abspath('../..'))
-#print(os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../tiledb'))
-print(os.path.abspath('../../tiledb'))
+# sys.path.insert(0, os.path.abspath('../..'))
+# print(os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../../tiledb"))
+print(os.path.abspath("../../tiledb"))
 
 # -- ReadTheDocs configuration ---------------------------------------------
 
 # Special handling on ReadTheDocs builds.
 # Some of this code is from https://github.com/robotpy/robotpy-docs/blob/master/conf.py
-readthedocs = os.environ.get('READTHEDOCS', None) == 'True'
-rtd_version = os.environ.get('READTHEDOCS_VERSION', 'latest')
-rtd_version = rtd_version if rtd_version in ['stable', 'latest'] else 'stable'
+readthedocs = os.environ.get("READTHEDOCS", None) == "True"
+rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+rtd_version = rtd_version if rtd_version in ["stable", "latest"] else "stable"
 
 # -- Project information -----------------------------------------------------
 
-project = 'TileDB-Cloud-Py'
-copyright = '2020, TileDB, Inc.'
-author = 'TileDB, Inc.'
+project = "TileDB-Cloud-Py"
+copyright = "2020, TileDB, Inc."
+author = "TileDB, Inc."
 
 
 # -- General configuration ---------------------------------------------------
@@ -41,26 +41,38 @@ author = 'TileDB, Inc.'
 extensions = [
     # 'sphinx.ext.autodoc',
     #'sphinxcontrib.apidoc',
-    'sphinx.ext.autodoc',  # Core library for html generation from docstrings
-    'sphinx.ext.autosummary',  # Create neat summary tables
+    "sphinx.ext.autodoc",  # Core library for html generation from docstrings
+    "sphinx.ext.autosummary",  # Create neat summary tables
 ]
 autosummary_generate = True
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
-master_doc = 'index'
+master_doc = "index"
 
-#apidoc_module_dir = '../..'
-#apidoc_excluded_paths = ['tests', 'tiledb/cloud/rest_api', 'setup.py', '**/test/**']
-#apidoc_separate_modules = True
+# apidoc_module_dir = '../..'
+# apidoc_excluded_paths = ['tests', 'tiledb/cloud/rest_api', 'setup.py', '**/test/**']
+# apidoc_separate_modules = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_autosummary/tiledb.test*", "_autosummary/tiledb.cloud.rest_api.test*", "_autosummary/tiledb.version.rst", "_autosummary/tiledb.array*", "_autosummary/tiledb.core*", "_autosummary/tiledb.dataframe*", "_autosummary/tiledb.highlevel", "_autosummary/tiledb.libtiledb*", "_autosummary/tiledb.metadata*", "_autosummary/tiledb.multirange*", "_autosummary/tiledb.version.rst"]
+exclude_patterns = [
+    "_autosummary/tiledb.test*",
+    "_autosummary/tiledb.cloud.rest_api.test*",
+    "_autosummary/tiledb.version.rst",
+    "_autosummary/tiledb.array*",
+    "_autosummary/tiledb.core*",
+    "_autosummary/tiledb.dataframe*",
+    "_autosummary/tiledb.highlevel",
+    "_autosummary/tiledb.libtiledb*",
+    "_autosummary/tiledb.metadata*",
+    "_autosummary/tiledb.multirange*",
+    "_autosummary/tiledb.version.rst",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -70,15 +82,18 @@ exclude_patterns = ["_autosummary/tiledb.test*", "_autosummary/tiledb.cloud.rest
 #
 # html_theme = 'alabaster'
 if readthedocs:
-    html_theme = 'default'
+    html_theme = "default"
 else:
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+html_sidebars = {
+    "**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"]
+}
