c379a64bd543f98278426a343b541534bc0da0ad
George Sakkis <george.sakkis@tiledb.com>
2021-02-05 17:21:37 +0200

    DAG.cancel: cancel also the not-started nodes

--- tests/test_delayed.py
+++ tests/test_delayed.py
@@ -111,22 +111,26 @@ class DelayedFailureTest(unittest.TestCase):
 
 class DelayedCancelTest(unittest.TestCase):
     def test_cancel(self):
+        node = Delayed(time.sleep, local=True, name="multi_node_2")(3)
+        node_2 = Delayed(np.mean, local=True, name="multi_node_2")([1, 1])
+        node_2.depends_on(node)
+
         with self.assertRaises(TimeoutError):
-            node = Delayed(time.sleep, local=True, name="multi_node_2")(100)
-            node_2 = Delayed(np.mean, local=True, name="multi_node_2")([1, 1])
-            node_2.depends_on(node)
-
-            # Set timeout to 1 second, the dependency on node will wait for 100 second, so we'll timeout and cancel
+            # Set timeout to 1 second, the dependency on node will wait for 3 seconds,
+            # so we'll timeout and cancel
             node_2.set_timeout(1)
-
             node_2.compute()
 
-            # Cancel DAG
-            node_2.dag.cancel()
+        node_2.dag.cancel()
 
-            self.assertEqual(node.result(), None)
-            self.assertEqual(node.status, Status.CANCELLED)
-            self.assertEqual(node_2.dag.status, Status.CANCELLED)
+        self.assertIs(node.dag, node_2.dag)
+        self.assertEqual(node.dag.status, Status.CANCELLED)
+
+        self.assertEqual(node.status, Status.CANCELLED)
+        self.assertEqual(node.result(), None)
+
+        self.assertEqual(node_2.status, Status.CANCELLED)
+        self.assertEqual(node_2.result(), None)
 
 
 class DelayedCloudApplyTest(unittest.TestCase):
