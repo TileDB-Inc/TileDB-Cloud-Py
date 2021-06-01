21ee199ac8dedd337a1aca8fcd87fafc12ae5aa4
Paul Fisher <paul.fisher@tiledb.com>
2021-06-01 19:54:34 +0000

    Copy signature of async array and UDF functions onto sync versions.
    This change introduces `signature_of`, which copies the function
    signature from a source to the decorated function, and applies it to
    the synchronous functions in array.py and udf.py.
    
    This avoids potential issues with parameter order or defaults falling
    out of sync between the synchronous and the async versions, while still
    allowing interactive users to access tab-completion. The docstrings
    of the synchronous versions link directly to the async versions.
    
    I have manually tested this both in IPython and Jupyter and tab
    completion and other contextual help continues to work there.

--- tiledb/cloud/utils.py
+++ tiledb/cloud/utils.py
@@ -1,6 +1,6 @@
 import inspect
 import logging
-from typing import Callable, Optional
+from typing import Callable, Optional, TypeVar
 
 logger = logging.getLogger("tiledb.cloud")
 
@@ -18,3 +18,30 @@ def getsourcelines(func: Callable) -> Optional[str]:
             exc,
         )
     return None
+
+
+_T = TypeVar("_T")
+
+
+def signature_of(src: Callable) -> Callable[[_T], _T]:
+    """Decorator that applies the signature of ``func`` to the wrapped function.
+
+    This allows autocomplete tools, like in Jupyter notebooks or IPython,
+    to use function signature information from the source function when
+    providing help for users about the destination function.
+
+    In this example, users will be able to see prompts for ``b`` or ``c``
+    when they press ``Tab`` while writing a call to  ``iter_a``:
+
+    >>> def a(b, c):
+    ...   return f'Hello, {b}! I am {c}.'
+    >>> @signature_of(a)
+    ... def iter_a(*args, **kwargs):
+    ...   yield a(*args, **kwargs)
+    """
+
+    def copy_to(dst):
+        dst.__signature__ = inspect.signature(src)
+        return dst
+
+    return copy_to
