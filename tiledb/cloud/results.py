978241d71ff01b4ed2c978ab7b1415ca4d51cd02
Paul Fisher <paul.fisher@tiledb.com>
2021-08-27 16:32:42 -0400

    Move decoding of results into classes in `results.py`.
    This moves most decoding logic into Decoder classes, which are designed
    to be easily pickleable for future use in stored parameters. It also
    introduces an early version of stored-parameter decoding in
    `dag/stored_params.py`, which will eventually be used (along with a
    reworking of async operations and DAG `Node`s) to avoid sending
    parameters back to the server.
    
    There are also some changes to make errors more explicit:
    
    - Rather than silently ignoring unknown result types, they are raised
      as errors.
    - Results returned as Pandas are now guaranteed to be returned as a
      DataFrame, rather than “a DataFrame, or potentially some other type”.

---
+++ tiledb/cloud/results.py
@@ -0,0 +1,54 @@
+"""Handles the tracking and decoding of task results."""
+
+import abc
+import dataclasses
+import json
+from typing import Any
+
+import cloudpickle
+import pandas
+import pyarrow
+from tiledb.cloud import tiledb_cloud_error as tce
+from tiledb.cloud.rest_api import models
+
+
+class AbstractDecoder(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def decode(data: bytes) -> Any:
+        raise NotImplementedError()
+
+
+def _load_arrow(data: bytes):
+    reader = pyarrow.RecordBatchStreamReader(data)
+    return reader.read_all()
+
+
+@dataclasses.dataclass(frozen=True)
+class Decoder(AbstractDecoder):
+
+    format: str
+
+    _DECODE_FNS = {
+        models.ResultFormat.NATIVE: cloudpickle.loads,
+        models.ResultFormat.JSON: json.loads,
+        models.ResultFormat.ARROW: _load_arrow,
+    }
+
+    def decode(self, data: bytes) -> Any:
+        try:
+            decoder = self._DECODE_FNS[self.format]
+        except KeyError:
+            raise tce.TileDBCloudError(f"{self.format!r} is not a valid result format.")
+        return decoder(data)
+
+
+@dataclasses.dataclass(frozen=True)
+class PandasDecoder(AbstractDecoder):
+
+    format: str
+
+    def decode(self, data: bytes) -> pandas.DataFrame:
+        if self.format == models.ResultFormat.ARROW:
+            reader = pyarrow.RecordBatchStreamReader(data)
+            return reader.read_pandas()
+        return pandas.DataFrame(Decoder(self.format).decode(data))
