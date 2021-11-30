f34ba0d068066b3821db9d411aa8987ab4b52771
Seth Shelnutt <seth@tiledb.io>
2021-11-29 22:11:26 -0500

    Support name to register created file with

--- tiledb/cloud/file.py
+++ tiledb/cloud/file.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from tiledb.cloud import client
 from tiledb.cloud import tiledb_cloud_error
 from tiledb.cloud.rest_api import ApiException as GenApiException
@@ -9,11 +11,13 @@ def create_file(
     namespace: str,
     input_uri: str,
     output_uri: str,
+    name: Optional[str] = None,
     async_req: bool = False,
 ) -> models.FileCreated:
     """
     Creates a TileDB file at the specified location
     :param namespace: namespace the create file operation belongs to
+    :param name: name to use for registration in TileDB Cloud
     :param input_uri: input file uri
     :param output_uri: output array uri
     :param async_req: return future instead of results for async support
@@ -25,6 +29,7 @@ def create_file(
         file_create = models.FileCreate(
             input_uri=input_uri,
             output_uri=output_uri,
+            name=name,
         )
 
         return api_instance.handle_create_file(
