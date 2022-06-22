eb49820b7f5af4184ffce808dbdf13daccc8fefd
Paul Fisher <paul.fisher@tiledb.com>
2022-06-22 17:16:11 -0400

    Prepare Nodes for retry functionality.
    This restructures the lifecycle handling of Nodes to be similar to
    that of the Executor, allowing it to be attempted multiple times.

--- tiledb/cloud/taskgraphs/client_executor/_base.py
+++ tiledb/cloud/taskgraphs/client_executor/_base.py
@@ -16,12 +16,16 @@ NOTHING = object()
 """Sentinel value to distinguish missing values from None."""
 
 
-class IClientExecutor(executor.Executor["Node"]):
+class IClientExecutor(executor.Executor["Node"], metaclass=abc.ABCMeta):
     """Executor sub-interface adding type information used by Nodes."""
 
     _client: client.Client
     _namespace: str
     _server_graph_uuid: Optional[uuid.UUID]
+
+    @abc.abstractmethod
+    def _enqueue_done_node(self, node: "Node") -> None:
+        raise NotImplementedError()
 
 
 ET = TypeVar("ET", bound=IClientExecutor)
@@ -47,6 +51,10 @@ class ParamFormat(enum.Enum):
     """
 
 
+_Self = TypeVar("_Self", bound="Node")
+"""Type for self-annotations where needed"""
+
+
 class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
     """Base class for Nodes to be executed locally.
 
@@ -55,7 +63,6 @@ class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self._event = threading.Event()
         self._status: Status = Status.WAITING
         self._result_exception: Optional[Exception] = None
         """An exception that was raised when executing the Node."""
@@ -66,65 +73,45 @@ class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
         by methods like `.exception()` rather than returned.
         """
 
-    # External API
+    #
+    # External API.
+    #
 
     def result(self, timeout: Optional[float] = None) -> _T:
-        self.wait(timeout)
-        # Because it's guaranteed that `exception` will *never be written to*
-        # after the `_event` is set, we don't need to hold a lock here.
-        exc = self._lifecycle_exception or self._result_exception
-        if exc:
-            raise exc
-        return self._result_impl()
+        with self._lifecycle_condition:
+            self._lifecycle_condition.wait_for(self._done, timeout)
+            exc = self._lifecycle_exception or self._result_exception
+            if exc:
+                raise exc
+            return self._result_impl()
 
     def exception(self, timeout: Optional[float] = None) -> Optional[Exception]:
-        self.wait(timeout)
-        if self._lifecycle_exception:
-            raise self._lifecycle_exception
-        return self._result_exception
+        with self._lifecycle_condition:
+            self._lifecycle_condition.wait_for(self._done, timeout)
+            if self._lifecycle_exception:
+                raise self._lifecycle_exception
+            return self._result_exception
 
     def cancel(self) -> bool:
-        cancelled = self._set_status_if_can_start(Status.CANCELLED)
-        if cancelled:
-            # If we were successful at cancelling this Node, we effectively
-            # "own" the result fields on this thread. We can set them safely
-            # ourselves before firing the Event.
+        with self._lifecycle_condition:
+            cancelled = self._set_status_if_can_start(Status.CANCELLED)
+            if not cancelled:
+                return cancelled
             self._lifecycle_exception = futures.CancelledError()
-            self._event.set()
-            self._do_callbacks()
+            self._lifecycle_condition.notify_all()
+            self.owner._enqueue_done_node(self)
+            cbs = self._callbacks()
+        if cancelled:
+            executor.do_callbacks(self, cbs)
         return cancelled
 
     def wait(self, timeout: Optional[float] = None) -> None:
-        wait_for(self._event, timeout)
+        with self._lifecycle_condition:
+            self._lifecycle_condition.wait_for(self._done, timeout)
 
-    # Internals
-
-    def _status_impl(self) -> Status:
-        return self._status
-
-    def _exec(self, parents: Dict[uuid.UUID, "Node"], input_value: Any) -> None:
-        """The boilerplate for the ``_exec`` implementation for local Nodes.
-
-        This handles all the lifecycle management for Node execution. It should
-        only ever be called by the Executor. Subclasses should instead implement
-        ``_exec_impl``, which contains the type-specific behavior.
-        """
-        if not self._set_status_if_can_start(Status.RUNNING):
-            return
-
-        try:
-            self._exec_impl(parents, input_value)
-        except Exception as ex:
-            with self._lifecycle_lock:
-                self._status = Status.FAILED
-                self._result_exception = ex
-            raise
-        else:
-            with self._lifecycle_lock:
-                self._status = Status.SUCCEEDED
-        finally:
-            self._event.set()
-            self._do_callbacks()
+    #
+    # Abstract methods to be implemented by subclasses to do the work of a Node.
+    #
 
     @abc.abstractmethod
     def _exec_impl(self, parents: Dict[uuid.UUID, "Node"], input_value: Any) -> None:
@@ -140,12 +127,63 @@ class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
         """
         raise NotImplementedError()
 
+    @abc.abstractmethod
+    def _encode_for_param(self, mode: ParamFormat) -> Any:
+        """Encodes the result of this node for use in a JSON parameter list.
+
+        This is used to pass the output of this Node into the parameters of a
+        following Node.
+        """
+        raise NotImplementedError()
+
+    #
+    # Execution internals
+    #
+
+    def _status_impl(self) -> Status:
+        return self._status
+
+    def _exec(self: _Self, parents: Dict[uuid.UUID, "Node"], input_value: Any) -> None:
+        """The boilerplate for the ``_exec`` implementation for local Nodes.
+
+        This handles all the lifecycle management for Node execution. It should
+        only ever be called by the Executor. Subclasses should instead implement
+        ``_exec_impl``, which contains the type-specific behavior.
+        """
+        with self._lifecycle_condition:
+            # Make sure we're still in a runnable state (weren't just cancelled)
+            if not self._set_status_if_can_start(Status.RUNNING):
+                return
+
+        try:
+            self._exec_impl(parents, input_value)
+        except Exception as ex:
+            with self._lifecycle_condition:
+                self._set_status_notify(Status.FAILED)
+                self._result_exception = ex
+                self.owner._enqueue_done_node(self)
+                cbs = self._callbacks()
+            raise
+        else:
+            with self._lifecycle_condition:
+                self._set_status_notify(Status.SUCCEEDED)
+                self._result_exception = None
+                self.owner._enqueue_done_node(self)
+                cbs = self._callbacks()
+        finally:
+            executor.do_callbacks(self, cbs)
+
+    #
+    # Lifecycle-management internals.
+    #
+
     def _assert_succeeded(self) -> None:
         if self._status is not Status.SUCCEEDED:
             raise AssertionError("_encoded_result is only valid for successful nodes")
 
     def _set_ready(self) -> None:
-        self._set_status_if_can_start(Status.READY)
+        with self._lifecycle_condition:
+            self._set_status_if_can_start(Status.READY)
 
     def _set_status_if_can_start(self, status: Status) -> bool:
         """If this node is allowed to start, updates its status.
@@ -153,19 +191,34 @@ class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
         Updates the status of this node, but only if it's unstarted (and is able
         to be started). Used to implement both :meth:`cancel` and the equivalent
         of ``Future.set_running_or_notify_cancel``.
-        """
-        with self._lifecycle_lock:
-            if self._status in (Status.WAITING, Status.READY):
-                self._status = status
-                return True
-            return False
 
-    def _set_parent_failed(self, pfe: executor.ParentFailedError) -> None:
-        failed = self._set_status_if_can_start(Status.PARENT_FAILED)
-        if failed:
+        ``_lifecycle_condition`` must be held.
+        """
+        if self._status in (Status.WAITING, Status.READY):
+            self._set_status_notify(status)
+            return True
+        return False
+
+    def _set_status_notify(self, status: Status) -> None:
+        """Sets the Node's status and notifies waiters.
+
+        ``_lifecycle_condition`` must be held.
+        """
+        if self._status is status:
+            return
+        self._status = status
+        self._lifecycle_condition.notify_all()
+
+    def _set_parent_failed(self: _Self, pfe: executor.ParentFailedError) -> None:
+        with self._lifecycle_condition:
+            could_set = self._set_status_if_can_start(Status.PARENT_FAILED)
+            if not could_set:
+                return
             self._lifecycle_exception = pfe
-            self._event.set()
-            self._do_callbacks()
+            self.owner._enqueue_done_node(self)
+            cbs = self._callbacks()
+
+        executor.do_callbacks(self, cbs)
 
     def _parent_failed_error(self) -> executor.ParentFailedError:
         """Returns the PFE that should be associated with this Node.
@@ -184,16 +237,18 @@ class Node(executor.Node[ET, _T], metaclass=abc.ABCMeta):
         if st is Status.PARENT_FAILED:
             assert isinstance(self._lifecycle_exception, executor.ParentFailedError)
             return self._lifecycle_exception
-        raise AssertionError(f"Accessing _parent_failed_error with status {st}")
+        raise AssertionError(f"Accessing {self}._parent_failed_error() with status {st}")
 
-    @abc.abstractmethod
-    def _encode_for_param(self, mode: ParamFormat) -> Any:
-        """Encodes the result of this node for use in a JSON parameter list.
-
-        This is used to pass the output of this Node into the parameters of a
-        following Node.
-        """
-        raise NotImplementedError()
+    def _prepare_to_retry(self) -> None:
+        with self._lifecycle_condition:
+            assert self._status in (
+                Status.FAILED,
+                Status.CANCELLED,
+                Status.PARENT_FAILED,
+            )
+            self._set_status_notify(Status.WAITING)
+            self._lifecycle_exception = None
+            self._result_exception = None
 
     def _to_log_metadata(
         self,
