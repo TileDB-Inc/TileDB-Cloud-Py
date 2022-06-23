c974ff0c617dc2e57b5912fd5e56f667b8d9b6bc
George Sakkis <george.sakkis@tiledb.com>
2022-06-23 17:06:18 +0300

    Update test_threaded_import (#278)
    * Update test_threaded_import to show it fails for tiledb<0.15.2 and succeeds otherwise
    
    * Bump tiledb to 0.15.2 for CI and update test_threaded_import

--- tests/test_threaded_import.py
+++ tests/test_threaded_import.py
@@ -5,32 +5,32 @@ from tiledb.tests.common import DiskTestCase
 from tiledb.tests.common import assert_subarrays_equal
 
 
-def threadtest_create_array(
-    uri,
-):
+def threadtest_create_array(uri):
     data = np.random.rand(20)
     schema = tiledb.libtiledb.schema_like(data)
     tiledb.Array.create(uri, schema)
-    with tiledb.DenseArray(uri, "w") as A:
+    with tiledb.open(uri, "w") as A:
         A[:] = data
 
 
 def threadtest_run_workers(uri):
-    def worker(n, uri):
-        with tiledb.DenseArray(uri) as A:
-            res = A.shape
-
-        return res
-
     import concurrent.futures
 
-    executor_cls = concurrent.futures.ThreadPoolExecutor
-    with executor_cls(max_workers=2) as executor:
-        futures = [executor.submit(worker, n, uri) for n in range(0, 5)]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(worker, uri) for _ in range(0, 5)]
         res = [f.result() for f in concurrent.futures.as_completed(futures)]
+        assert all(res), res
 
 
-class ThreadedImportTest(DiskTestCase):
+def worker(uri):
+    with tiledb.open(uri) as A:
+        pass
+    from tiledb.cloud.cloudarray import CloudArray
+
+    return isinstance(A, CloudArray)
+
+
+class TestThreadedImport(DiskTestCase):
     def test_threaded_import(self):
         import multiprocessing as mp
 
@@ -45,9 +45,5 @@ class ThreadedImportTest(DiskTestCase):
         p2 = mpctx.Process(target=threadtest_run_workers, args=(uri,))
         p2.start()
         p2.join()
+        # fixed by https://github.com/TileDB-Inc/TileDB-Py/pull/1096
         self.assertEqual(p2.exitcode, 0)
-
-        with tiledb.DenseArray(uri) as A:
-            from tiledb.cloud.cloudarray import CloudArray
-
-            self.assertTrue(CloudArray in A.__class__.__bases__)
