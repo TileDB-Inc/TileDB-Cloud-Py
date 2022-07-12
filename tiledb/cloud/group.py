996e9ff42407ce55d044739c08e2962abb35ba18
John Kerl <kerl.john.r@gmail.com>
2022-07-12 14:42:42 -0400

    iterating (incompletely) on code-review feedback

--- tiledb/cloud/group.py
+++ tiledb/cloud/group.py
@@ -1,31 +1,31 @@
-from typing import Dict, List
+from typing import Dict, List, Optional, Tuple
 
-from tiledb.cloud import client
-from tiledb.cloud import tiledb_cloud_error
-from tiledb.cloud import utils
-from tiledb.cloud.array import delete_array
-from tiledb.cloud.rest_api import ApiException as GenApiException
-
-# Re-export for compatibility.
-split_uri = utils.split_uri
+import tiledb.cloud.array
+import tiledb.cloud.client as cloud_client
+import tiledb.cloud.rest_api as rest_api
+import tiledb.cloud.rest_api.models as models
+import tiledb.cloud.tiledb_cloud_error as tiledb_cloud_error
+import tiledb.cloud.utils
 
 
-def get_group(uri):
+def get_group(uri: str) -> models.group_info.GroupInfo:
     """
     Returns the cloud metadata
 
     :return: metadata object
     """
-    (namespace, group_name) = split_uri(uri)
-    api_instance = client.client.groups_api
+    (namespace, group_name) = tiledb.cloud.utils.split_uri(uri)
+    api_instance = cloud_client.client.groups_api
 
     try:
-        return api_instance.get_group(namespace, group_name, async_req=async_req)
-    except GenApiException as exc:
+        return api_instance.get_group(namespace, group_name)
+    except rest_api.ApiException as exc:
         raise tiledb_cloud_error.check_exc(exc) from None
 
 
-def delete_group_recursively(uri, dry_run=False, verbose=False):
+def delete_group_recursively(
+    uri: str, dry_run: bool = False, verbose: bool = False
+) -> None:
     """
     Deletes the group object, after recursively deregistering/deleting child array elemens
     and deleting child group elements.
@@ -43,18 +43,21 @@ def delete_group_recursively(uri, dry_run=False, verbose=False):
     if dry_run:
         verbose = True
 
-    (namespace, group_name) = split_uri(uri)
+    (namespace, group_name) = tiledb.cloud.utils.split_uri(uri)
 
     # Get all members before deleting any, since we don't want to be deleting
     # things we're iterating over.
-    group_element_tiledb_uris = _get_group_element_tiledb_uris(uri)
+    (
+        array_element_tiledb_uris,
+        group_element_tiledb_uris,
+    ) = _get_group_element_tiledb_uris(uri)
 
-    for e in group_element_tiledb_uris["arrays"]:
+    for e in array_element_tiledb_uris:
         if verbose:
             print("DELETE ARRAY", e)
         if not dry_run:
-            delete_array(e)
-    for e in group_element_tiledb_uris["groups"]:
+            tiledb.cloud.array.delete_array(e)
+    for e in group_element_tiledb_uris:
         # Depth-first traversal
         delete_group_recursively(e, dry_run=dry_run, verbose=verbose)
         if verbose:
@@ -66,29 +69,28 @@ def delete_group_recursively(uri, dry_run=False, verbose=False):
 def delete_group(uri: str) -> None:
     """
     Deletes the group. The assets are not deleted nor are not relocated to any other group.
-    This method makes a synchronous HTTP request by default.
 
     You probably want `delete_group_recursively`.
 
     :param uri: TileDB Cloud URI of the group to be deleted
     """
-    (namespace, group_name) = split_uri(uri)
-    api_instance = client.client.groups_api
+    (namespace, group_name) = tiledb.cloud.utils.split_uri(uri)
+    api_instance = cloud_client.client.groups_api
     try:
         return api_instance.delete_group(namespace, group_name)
-    except GenApiException as exc:
+    except rest_api.ApiException as exc:
         raise tiledb_cloud_error.check_exc(exc) from None
 
 
-def _get_group_element_tiledb_uris(uri: str) -> Dict[str, List[str]]:
+def _get_group_element_tiledb_uris(uri: str) -> Tuple[List[str], List[str]]:
     """
     Given a TileDB Cloud group URI, returns a listing of array-element URIs,
     and group-element URIs. This is intended as an auxiliary helper method
     for `delete_group_recursively`.
     """
 
-    (namespace, group_name) = split_uri(uri)
-    api_instance = client.client.groups_api
+    (namespace, group_name) = tiledb.cloud.utils.split_uri(uri)
+    api_instance = cloud_client.client.groups_api
 
     try:
         array_tiledb_uris = []
@@ -96,7 +98,10 @@ def _get_group_element_tiledb_uris(uri: str) -> Dict[str, List[str]]:
         page_index = 1
         while True:
             page = api_instance.get_group_contents(
-                namespace, group_name, page=page_index
+                namespace,
+                group_name,
+                page=page_index,
+                per_page=20,
             )
             for e in page.entries:
                 # Do not do:
@@ -121,7 +126,7 @@ def _get_group_element_tiledb_uris(uri: str) -> Dict[str, List[str]]:
             if len(page.entries) == 0:
                 break
             page_index += 1
-        return {"arrays": array_tiledb_uris, "groups": group_tiledb_uris}
+        return (array_tiledb_uris, group_tiledb_uris)
 
-    except GenApiException as exc:
+    except rest_api.ApiException as exc:
         raise tiledb_cloud_error.check_exc(exc) from None
