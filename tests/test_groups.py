a280daa5a48e4b04b9f4340537a65863a440b4dc
John Kerl <kerl.john.r@gmail.com>
2022-07-14 22:25:22 -0400

    tests/test_groups.py

---
+++ tests/test_groups.py
@@ -0,0 +1,33 @@
+import tiledb
+import pandas as pd
+
+#def make_name(base):
+#    lowers = "abcdefghijklmnopqrstuvwxyz"
+#    length = 12
+#    word = ""
+#    for j in range(0, length):
+#        word += random.choice(lowers)
+#    return base + "-" + word
+
+# def make_nested_group():
+#     make array a2
+#     make array a3
+#     make group g2
+#     add a2,a3 to g2
+#     make array a1
+#     make group g1
+#     add g2,a1 to g1
+
+# test get_group
+# test _get_group_element_tiledb_uris
+# test delete_group_recursively
+
+# tiledb.from_pandas(
+#     uri=self.uri,
+#     dataframe=dataframe,
+#     name=self.name,
+# )
+
+# return pd.DataFrame.from_dict(
+#     {k: _to_tiledb_supported_array_type(v) for k, v in x.items()}
+# )
