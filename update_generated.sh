8c10ac4caa32aa6359e03311cd19a557135bcf78
Paul Fisher <paul.fisher@tiledb.com>
2022-10-26 14:50:34 -0400

    Update API generator to prepare for generating multiple files.
    This breaks the API generator into functions so that it can be run
    against both specification versions.

--- update_generated.sh
+++ update_generated.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 USAGE='
 Usage:
 
-    update_generated.sh /path/to/TileDB-Cloud-API-Spec/openapi-v1.yaml
+    update_generated.sh /path/to/TileDB-Cloud-API-Spec
 
 This will create generated API documents in [repo root]/tiledb/cloud/rest_api.
 '
@@ -16,25 +16,10 @@ if [[ "$#" -ne 1 ]]; then
 fi
 
 ROOT="$(git rev-parse --show-toplevel)"
-
 GENERATOR="$ROOT/.cache/openapi-generator-cli-4.3.1.jar"
-
-if [[ ! -f "$GENERATOR" ]]; then
-  mkdir -p "$ROOT/.cache"
-  wget -O "$GENERATOR" "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar"
-fi
-
-SPEC="$1"
 TEMP_PACKAGE_NAME="zzz_replace_me_zzz"
-PACKAGE_NAME="rest_api"
 
-TARGET_PATH="$ROOT/tiledb/cloud"
-
-TEMP_PATH="$(mktemp -d /tmp/api_gen.XXXXXX)"
-
-################################################################################
-# minimize output to what we want
-cat <<EOF > ${TEMP_PATH}/.openapi-generator-ignore
+OPENAPI_GENERATOR_IGNORE='
 docs/*
 test/
 test/*
@@ -44,80 +29,106 @@ test-requirements.txt
 setup.py
 .gitignore
 .travis.yml
-EOF
+'
 
-################################################################################
-# openapi-generator config
-cat <<EOF > ${TEMP_PATH}/openapi_config-api
-  {
+OPENAPI_GENERATOR_CONFIG='
+{
   "projectName": "tiledb-cloud",
-  "packageName": "${TEMP_PACKAGE_NAME}",
+  "packageName": "'"${TEMP_PACKAGE_NAME}"'",
   "documentationPage": false,
   "generateSourceCodeOnly": true
-  }
-EOF
+}
+'
 
-################################################################################
-java -jar "$GENERATOR" \
-  generate \
-  -c "$TEMP_PATH/openapi_config-api" \
-  -o "$TEMP_PATH" \
-  -i "$SPEC" \
-  -g python
-
-# Rewrite imports and links in docs,
-# and work around https://github.com/OpenAPITools/openapi-generator/issues/10236
-find "${TEMP_PATH}" \
-  -type f \
-  -execdir \
-    sed --in-place \
-      -e "s/${TEMP_PACKAGE_NAME}\./tiledb.cloud.${PACKAGE_NAME}./g" \
-      -e "s/\\(from\\|import\\) ${TEMP_PACKAGE_NAME}/\\1 tiledb.cloud.${PACKAGE_NAME}/g" \
-      -e "s/${TEMP_PACKAGE_NAME}/${PACKAGE_NAME}/g" \
-      -e "s/ del =/ _del =/g" \
-      '{}' \
-  '+'
-
-# Fix up links in README.
-sed --in-place -e "s/](rest_api\\//](/g" "${TEMP_PATH}/${TEMP_PACKAGE_NAME}_README.md"
-
-# Get TARGET_PATH out of the way (if present).
-if [ -e "$TARGET_PATH/$PACKAGE_NAME" ]; then
-  rm -r -- "$TARGET_PATH/$PACKAGE_NAME"
-fi
-
-# move generated files to TARGET_PATH
-cp -r "${TEMP_PATH}/${TEMP_PACKAGE_NAME}" "${TARGET_PATH}/${PACKAGE_NAME}"
-
-cp ${TEMP_PATH}/${TEMP_PACKAGE_NAME}_README.md ${TARGET_PATH}/${PACKAGE_NAME}/README.md
-cp ${TEMP_PATH}/.openapi-generator-ignore ${TARGET_PATH}/${PACKAGE_NAME}/
-cp ${TEMP_PATH}/openapi_config-api ${TARGET_PATH}/${PACKAGE_NAME}/
-cp -r ${TEMP_PATH}/.openapi-generator/ ${TARGET_PATH}/${PACKAGE_NAME}/
-
-echo
-echo "Output copied from '${TEMP_PATH}' to '${TARGET_PATH}/${PACKAGE_NAME}'"
-
-for command in isort black; do
-  if ! [[ -x "$(command -v "${command}")" ]]; then
-    echo "${command} is not installed." >&2
-    echo 'Before sending a review request, run:' >&2
-    echo "  $ pip install ${command}" >&2
-    echo "  $ ${command} ${ROOT}" >&2
-    echo 'to ensure your files are correctly formatted.' >&2
-  else
-    "${command}" "${ROOT}"
+download_generator() {
+  if [[ ! -f "$GENERATOR" ]]; then
+    mkdir -p "$ROOT/.cache"
+    wget -O "$GENERATOR" "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar"
   fi
-done
+}
+
+generate_api() {
+  SPEC="$1"
+  PACKAGE_NAME="$2"
+  PACKAGE_NAME_SLASHY="${PACKAGE_NAME//.//}"
+
+  TARGET_PATH="$ROOT/tiledb/cloud"
+
+  TEMP_PATH="$(mktemp -d /tmp/api_gen.XXXXXX)"
+
+  ################################################################################
+  # minimize output to what we want
+  echo "$OPENAPI_GENERATOR_IGNORE" > ${TEMP_PATH}/.openapi-generator-ignore
+
+  ################################################################################
+  # openapi-generator config
+  echo "$OPENAPI_GENERATOR_CONFIG" > ${TEMP_PATH}/openapi_config-api
+
+  ################################################################################
+  java -jar "$GENERATOR" \
+    generate \
+    -c "$TEMP_PATH/openapi_config-api" \
+    -o "$TEMP_PATH" \
+    -i "$SPEC" \
+    -g python
+
+  # Rewrite imports and links in docs,
+  # and work around https://github.com/OpenAPITools/openapi-generator/issues/10236
+  find "${TEMP_PATH}" \
+    -type f \
+    -execdir \
+      sed --in-place \
+        -e "s/${TEMP_PACKAGE_NAME}\./tiledb.cloud.${PACKAGE_NAME}./g" \
+        -e "s/\\(from\\|import\\) ${TEMP_PACKAGE_NAME}/\\1 tiledb.cloud.${PACKAGE_NAME}/g" \
+        -e "s/${TEMP_PACKAGE_NAME}/${PACKAGE_NAME}/g" \
+        -e "s/ del =/ _del =/g" \
+        '{}' \
+    '+'
+
+  # Fix up links in README.
+  sed --in-place -e "s/](${PACKAGE_NAME}\\//](/g" "${TEMP_PATH}/${TEMP_PACKAGE_NAME}_README.md"
+
+  # Get TARGET_PATH out of the way (if present).
+  if [ -e "$TARGET_PATH/$PACKAGE_NAME_SLASHY" ]; then
+    rm -r -- "$TARGET_PATH/$PACKAGE_NAME_SLASHY"
+  fi
+
+  # move generated files to TARGET_PATH
+  cp -r "${TEMP_PATH}/${TEMP_PACKAGE_NAME}" "${TARGET_PATH}/${PACKAGE_NAME_SLASHY}"
+
+  cp ${TEMP_PATH}/${TEMP_PACKAGE_NAME}_README.md ${TARGET_PATH}/${PACKAGE_NAME_SLASHY}/README.md
+  cp ${TEMP_PATH}/.openapi-generator-ignore ${TARGET_PATH}/${PACKAGE_NAME_SLASHY}/
+  cp ${TEMP_PATH}/openapi_config-api ${TARGET_PATH}/${PACKAGE_NAME_SLASHY}/
+  cp -r ${TEMP_PATH}/.openapi-generator/ ${TARGET_PATH}/${PACKAGE_NAME_SLASHY}/
+
+  echo
+  echo "Output copied from '${TEMP_PATH}' to '${TARGET_PATH}/${PACKAGE_NAME_SLASHY}'"
+}
+
+run_format() {
+  for command in isort black; do
+    if ! [[ -x "$(command -v "${command}")" ]]; then
+      echo "${command} is not installed." >&2
+      echo 'Before sending a review request, run:' >&2
+      echo "  $ pip install ${command}" >&2
+      echo "  $ ${command} ${ROOT}" >&2
+      echo 'to ensure your files are correctly formatted.' >&2
+    else
+      "${command}" "${ROOT}"
+    fi
+  done
+}
 
 # Apply an api_client patch to avoid descending into known–JSON-safe values.
-git apply - <<EOF
+apply_json_safe_patch() {
+  git apply - <<EOF
 diff --git a/tiledb/cloud/rest_api/api_client.py b/tiledb/cloud/rest_api/api_client.py
 index 267385d..6d244a0 100644
 --- a/tiledb/cloud/rest_api/api_client.py
 +++ b/tiledb/cloud/rest_api/api_client.py
 @@ -25,6 +25,7 @@ from dateutil.parser import parse
  from six.moves.urllib.parse import quote
- 
+
  import tiledb.cloud.rest_api.models
 +from tiledb.cloud._common import json_safe
  from tiledb.cloud.rest_api import rest
@@ -134,3 +145,11 @@ index 267385d..6d244a0 100644
          elif isinstance(obj, list):
 EOF
 
+}
+
+ABSPATH="$(realpath "$1")"
+
+download_generator
+generate_api "${ABSPATH%/}/openapi-v1.yaml" rest_api
+run_format
+apply_json_safe_patch
