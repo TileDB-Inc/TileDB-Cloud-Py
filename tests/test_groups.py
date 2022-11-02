a1e64f374b2897d71da52c83f31d851217cc848c
paul fisher <paul.fisher@tiledb.com>
2022-11-02 16:45:42 -0400

    Implement groups API creation/deletion functions. (#348)
    Adds:
    - Create new group.
    - Register existing group.
    - Deregister group.

---
+++ tests/test_groups.py
@@ -0,0 +1,62 @@
+import time
+import unittest
+
+from tiledb.cloud import client
+from tiledb.cloud import groups
+from tiledb.cloud import rest_api
+from tiledb.cloud import testonly
+
+TRIES = 5
+
+
+class GroupsTest(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.namespace, storage_path, _ = groups._default_ns_path_cred()
+        self.test_path = storage_path + "/" + testonly.random_name("groups_test")
+
+    def test_create_deregister(self):
+        # TODO: This test leaves detritus around.
+        # Once we get true delete functions, clean that up.
+        outer_name = testonly.random_name("outer")
+        outer_storage_name = testonly.random_name(outer_name)
+        outer_storage_path = f"{self.test_path}/{outer_storage_name}"
+        outer_uri = f"tiledb://{self.namespace}/{outer_name}"
+        groups.create(outer_name, storage_uri=outer_storage_path)
+        self.assert_group_exists(outer_name)
+
+        groups.deregister(outer_uri)
+        self.assert_group_not_exists(outer_name)
+
+        groups.register(outer_storage_path, name=outer_name)
+        self.assert_group_exists(outer_name)
+        inner_name = testonly.random_name("inner")
+        groups.create(inner_name, parent_uri=outer_uri)
+        self.assert_group_exists(inner_name)
+        groups.deregister(outer_uri, recursive=True)
+        self.assert_group_not_exists(outer_name)
+        self.assert_group_not_exists(inner_name)
+
+    def assert_group_exists(self, name):
+        """Asserts that a group exists, and gives it a few tries."""
+        api = client.build(rest_api.GroupsApi)
+        for _ in range(TRIES):
+            try:
+                api.get_group_contents(group_namespace=self.namespace, group_name=name)
+                return
+            except rest_api.ApiException:
+                pass  # try again
+            time.sleep(1)
+        self.fail(f"group {self.namespace}/{name} does not exist")
+
+    def assert_group_not_exists(self, name):
+        """Asserts that a group does not exist, giving it a few tries to go."""
+        api = client.build(rest_api.GroupsApi)
+        for _ in range(TRIES):
+            try:
+                api.get_group_contents(group_namespace=self.namespace, group_name=name)
+            except rest_api.ApiException as apix:
+                if apix.status in (400, 404):
+                    return
+            time.sleep(1)
+        self.fail(f"group {self.namespace}/{name} still exists")
---
+++ tiledb/cloud/groups.py
@@ -0,0 +1,180 @@
+"""Functions for managing TileDB Cloud groups."""
+
+import posixpath
+import urllib.parse
+from typing import Iterable, Optional, Tuple, Union
+
+import tiledb.cloud.tiledb_cloud_error as tce
+from tiledb.cloud import client
+from tiledb.cloud import rest_api
+from tiledb.cloud import utils
+from tiledb.cloud._common import api_v2
+
+
+def create(
+    name: str,
+    *,
+    namespace: Optional[str] = None,
+    parent_uri: Optional[str] = None,
+    storage_uri: Optional[str] = None,
+    credentials_name: Optional[str] = None,
+) -> None:
+    """Creates a new TileDB Cloud group.
+
+    :param name: The name of the group to create.
+    :param namespace: The namespace to create the group in.
+        If not provided, the current logged-in user will be used.
+    :param parent_uri: The parent URI to add the group to, if desired.
+    :param storage_uri: The backend URI where the group will be stored.
+        If not provided, uses the namespace's default storage path for groups.
+    :param credentials_name: The name of the storage credential to use for
+        creating the group. If not provided, uses the namespace's default
+        credential for groups.
+    """
+    if not (namespace and storage_uri and credentials_name):
+        namespace, default_path, default_cred = _default_ns_path_cred(namespace)
+        storage_uri = storage_uri or (default_path + "/" + name)
+        credentials_name = credentials_name or default_cred
+
+    groups_client = client.build(api_v2.GroupsApi)
+    groups_client.create_group(
+        group_namespace=namespace,
+        x_tiledb_cloud_access_credentials_name=credentials_name,
+        group_creation=api_v2.GroupCreationRequest(
+            group_details=api_v2.GroupCreationRequestGroupDetails(
+                name=name,
+                uri=storage_uri,
+            )
+        ),
+    )
+    if parent_uri:
+        _add_to(namespace=namespace, name=name, parent_uri=parent_uri)
+
+
+def register(
+    storage_uri: str,
+    *,
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    credentials_name: Optional[str] = None,
+    parent_uri: Optional[str] = None,
+):
+    """Registers a pre-existing group."""
+    if not (namespace and credentials_name):
+        namespace, _, default_cred = _default_ns_path_cred(namespace)
+        credentials_name = credentials_name or default_cred
+
+    if not name:
+        # Extract the basename from the storage URI and use it for the name.
+        parsed_uri = urllib.parse.urlparse(storage_uri)
+        name = posixpath.basename(parsed_uri.path)
+    groups_client = client.build(api_v2.GroupsApi)
+    groups_client.register_group(
+        group_namespace=namespace,
+        x_tiledb_cloud_access_credentials_name=credentials_name,
+        group_registration=api_v2.GroupRegistrationRequest(
+            group_details=api_v2.GroupRegistrationRequestGroupDetails(
+                name=name,
+                uri=storage_uri,
+                access_credentials_name=credentials_name,
+                parent=parent_uri,
+            )
+        ),
+    )
+    if parent_uri:
+        _add_to(namespace=namespace, name=name, parent_uri=parent_uri)
+
+
+def deregister(
+    uri: str,
+    *,
+    recursive: bool = False,
+) -> None:
+    """Deregisters the given group from TileDB Cloud.
+
+    :param uri: The URI of the group to deregister.
+    :param recursive: If true, deregister the group recursively by deregistering
+        all of the elements of the group (and all elements of those groups,
+        recursively) before deregistering the group itself.
+    """
+    namespace, name = utils.split_uri(uri)
+
+    array_api = client.build(rest_api.ArrayApi)
+    groups_api = client.build(api_v2.GroupsApi)
+    groups_api_v1 = client.build(rest_api.GroupsApi)
+    if recursive:
+        while True:
+            contents: rest_api.GroupContents = groups_api_v1.get_group_contents(
+                group_namespace=namespace,
+                group_name=name,
+                page=1,
+                per_page=100,
+            )
+            pm: rest_api.PaginationMetadata = contents.pagination_metadata
+            if not pm.total_items:
+                break  # Zero total items remain -> we're done.
+            members: Iterable[rest_api.GroupEntry] = contents.entries
+            for m in members:
+                if m.array:
+                    arr: rest_api.ArrayInfo = m.array
+                    array_api.deregister_array(
+                        namespace=arr.namespace,
+                        array=arr.id,
+                    )
+                elif m.group:
+                    grp: rest_api.GroupInfo = m.group
+                    deregister(grp.tiledb_uri, recursive=recursive)
+                else:
+                    raise tce.TileDBCloudError("unexpected group member type")
+    groups_api.deregister_group(group_namespace=namespace, group_name=name)
+
+
+def _default_ns_path_cred(namespace: Optional[str] = None) -> Tuple[str, str, str]:
+    principal: Union[rest_api.User, rest_api.Organization]
+    if namespace:
+        try:
+            principal = client.build(rest_api.UserApi).get_user_with_username(namespace)
+        except rest_api.ApiException:
+            try:
+                principal = client.build(rest_api.OrganizationApi).get_organization(
+                    namespace
+                )
+            except rest_api.ApiException:
+                raise ValueError(f"Could not find namespace {namespace!r}.")
+    else:
+        principal = client.default_user()
+        namespace = principal.username
+        assert namespace
+
+    locs: Optional[rest_api.AssetLocations] = principal.asset_locations
+    # Weird structure to silence mypy complaints.
+    storage: Optional[rest_api.StorageLocation] = locs.groups if locs else None
+
+    path: Optional[str] = None
+    cred_name: Optional[str] = None
+    if storage:
+        path = storage.path
+        cred_name = storage.credentials_name
+
+    path = path or (principal.default_s3_path + "/groups")
+    cred_name = cred_name or principal.default_s3_path_credentials_name
+    return namespace, path, cred_name
+
+
+def _add_to(*, namespace: str, name: str, parent_uri: str) -> None:
+    parent_ns, parent_name = utils.split_uri(parent_uri)
+    client.build(api_v2.GroupsApi).update_group_contents(
+        group_namespace=parent_ns,
+        group_name=parent_name,
+        group_update_contents=api_v2.GroupContentsChangesRequest(
+            group_changes=api_v2.GroupContentsChangesRequestGroupChanges(
+                members_to_add=[
+                    api_v2.GroupMember(
+                        name=name,
+                        uri=f"tiledb://{namespace}/{name}",
+                        type="GROUP",
+                    ),
+                ],
+            ),
+        ),
+    )
