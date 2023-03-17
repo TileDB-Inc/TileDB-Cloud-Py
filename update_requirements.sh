7b4ff76fba48ffaff5a0a08cde027bfce6b3611f
Paul Fisher <paul.fisher@tiledb.com>
2023-03-17 14:42:41 -0400

    add xarray as a test dependency and regen requirements

--- update_requirements.sh
+++ update_requirements.sh
@@ -9,7 +9,9 @@ write-requirements() {
   conda create -y -p "$SCRATCH/conda" "python=$VERSION"
   "$SCRATCH/conda/bin/python" -m venv "$SCRATCH/venv"
   "$SCRATCH/venv/bin/pip" install --upgrade pip wheel
-  "$SCRATCH/venv/bin/pip" install .
+  # xarray is a test-only direct dependency for us.
+  # It is normally just a transitive optional dependency of Pandas.
+  "$SCRATCH/venv/bin/pip" install . xarray
   "$SCRATCH/venv/bin/pip" freeze --exclude tiledb-cloud >"requirements-py$VERSION.txt"
 }
 
