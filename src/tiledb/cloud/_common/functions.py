ab696fdae5e1054b6612aac8ec4fbb9ebdaaab18
paul fisher <paul.fisher@tiledb.com>
2023-04-24 11:21:06 -0400

    Add tool to force function pickling by value. (#391)
    When registering certain UDFs, like `trampoline.run_python_function`,
    we want to ensure that they're registered by reference, not by value.
    Removing the `__module__` from the function is the simplest way to
    make that happen with the current version of cloudpickle.
    
    This may be worth exporting in the future, or alternately even using it
    in the default UDF execution/registration process, but until we have
    a better idea of how to work with it, I'd like it to stay internal-only.

--- src/tiledb/cloud/_common/functions.py
+++ src/tiledb/cloud/_common/functions.py
@@ -1,6 +1,7 @@
 """Utilities for dealing with functions for serialization and registration."""
 
 import inspect
+import types
 from typing import Callable, Optional, TypeVar, Union
 
 _T = TypeVar("_T")
@@ -8,6 +9,66 @@ Funcable = Union[str, Callable[..., _T]]
 """Either a Python function or the name of a registered UDF."""
 _builtin_function = type(len)
 """The type of functions implemented in C."""
+
+
+def to_register_by_value(__fn: types.FunctionType) -> types.FunctionType:
+    """Converts the given function into one that can be registered "by value".
+
+    When a function is serialized by reference, the result is roughly equivalent
+    to ``import module.where.it.lives; module.where.it.lives.the_fn(...)``.
+    When it is serialized by value, the function's (compiled) code is serialized
+    and used at the receiving end to execute directly.
+
+    This by itself does not *guarantee* that a function pickled by value will be
+    usable. The same restrictions apply as to existing functions. For instance,
+    if the function refers to globals from a module not importable at the
+    destination, it still won't work.
+    """
+
+    if not isinstance(__fn, types.FunctionType):
+        raise TypeError(
+            "Only regular functions (not methods or built-in functions)"
+            " may be converted to register-by-value."
+        )
+
+    # The value of a __module__ is (part of) how cloudpickle decides whether
+    # to serialize a function by reference or by value.  It assumes that if
+    # a function has a __module__ and is available in that module, it should
+    # be importable on the receiving end (with some exceptions).  If it has no
+    # __module__, or the __module__ is "__main__", it will always serialize
+    # by value.
+    #
+    # This is a fancy way of copying everything but a function's __module__
+    # to a new function object, without mutating the original.
+    # Alas, copy.copy and copy.deepcopy both return the original instance
+    # when used to copy a function object.
+
+    new_fn = types.FunctionType(
+        __fn.__code__, globals=__fn.__globals__, closure=__fn.__closure__
+    )
+    # We don't want the two objects to share the same __dict__ reference.
+    new_fn.__dict__.update(__fn.__dict__)
+    new_fn.__module__ = "<pickled data>"  # fictitious.
+    # Special function attributes:
+    # https://docs.python.org/3.11/reference/datamodel.html#index-33
+    for attr in (
+        "__doc__",
+        "__name__",
+        "__qualname__",
+        # __module__: already set
+        "__defaults__",
+        # __code__: already set
+        # __globals__: read-only, already set
+        # __dict__: copied entry-by-entry above
+        # __closure__: read-only, already set
+        "__annotations__",
+        "__kwdefaults__",
+    ):
+        try:
+            setattr(new_fn, attr, getattr(__fn, attr))
+        except AttributeError:
+            pass  # It's fine if it's missing or unset on the original.
+    return new_fn
 
 
 def check_funcable(**kwargs) -> None:
