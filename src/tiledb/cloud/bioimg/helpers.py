cc3f88b698056f992a2333199282b9eee7ea3643
ktsitsi <kostas.t@tiledb.com>
2023-06-26 11:26:27 +0300

    Adding exportation DAG functionality for TileDB BioImg

---
+++ src/tiledb/cloud/bioimg/helpers.py
@@ -0,0 +1,64 @@
+import math
+import os
+from typing import Any, Iterator, Mapping, Sequence, Tuple
+
+import tiledb
+
+
+def get_uris(source: Sequence[str], output_dir: str, config: Mapping[str, Any]):
+    """Match input uri/s with output destinations
+
+    :param source: A sequence of paths or path to input
+    :param output_dir: A path to the output directory
+    """
+    vfs = tiledb.VFS(config=config)
+
+    def create_output_path(input_file, output_dir) -> str:
+        return os.path.join(output_dir, os.path.basename(input_file) + ".tdb")
+
+    def iter_paths(sequence) -> Iterator[Tuple]:
+        for uri in sequence:
+            yield uri, create_output_path(uri, output_dir)
+
+    if len(source) == 1 and vfs.is_dir(source[0]):
+        # Folder like input
+        return tuple(iter_paths(vfs.ls(source[0])))
+    elif isinstance(source, Sequence):
+        # List of input uris - single file is one element list
+        return tuple(iter_paths(source))
+
+
+def serialize_filter(filter):
+    if isinstance(filter, tiledb.Filter):
+        filter_dict = filter._attrs_()
+        filter_dict["_name"] = type(filter).__name__
+        return filter_dict
+    else:
+        raise TypeError
+
+
+def batch(iterable, chunks):
+    # Iterator for prividing batches of chunks
+    length = len(iterable)
+    for ndx in range(0, length, chunks):
+        yield iterable[ndx : min(ndx + chunks, length)]
+
+
+def scale_calc(source, num_batches):
+    """Calculate scaling settings for batch_size and max_workers
+
+    :param source: The source iterable containing files to be ingested/exported
+    :param num_batches: The number of batches given by the API
+    :return: Tuple batch_size, max_workers
+    """
+    # If num_batches is default create number of images nodes
+    # constraint node max_workers to 20 fully heuristic
+    if num_batches is None:
+        num_batches = len(source)
+        batch_size = 1
+        max_workers = 20
+    else:
+        batch_size = math.ceil(len(source) / num_batches)
+        max_workers = None
+
+    return batch_size, max_workers
