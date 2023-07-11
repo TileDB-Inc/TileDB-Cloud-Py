3fef4ce208c916940428337cf1e1d9f66803518c
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2023-07-11 18:38:07 +0300

    Hotfix of failing bioimaging ingestion node (#431)

--- src/tiledb/cloud/bioimg/ingestion.py
+++ src/tiledb/cloud/bioimg/ingestion.py
@@ -169,7 +169,11 @@ def get_uris(source: Sequence[str], output_dir: str, config: Mapping[str, Any]):
 
     if len(source) == 1 and vfs.is_dir(source[0]):
         # Folder like input
-        return tuple(iter_paths(vfs.ls(source[0])))
+        contents = vfs.ls(source[0])
+        if len(contents) == 1:
+            # Only the root and no contents
+            raise ValueError("Input bucket should contain images for ingestion")
+        return tuple(iter_paths(contents[1:]))
     elif isinstance(source, Sequence):
         # List of input uris - single file is one element list
         return tuple(iter_paths(source))
