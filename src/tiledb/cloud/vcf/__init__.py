14cca1e873993bb2bc5d912260444ca61d16ba75
George Powley <george.powley@gmail.com>
2023-07-18 19:18:07 -0400

    Consolidate VCF stats arrays at the end of ingestion (#432)

--- src/tiledb/cloud/vcf/__init__.py
+++ src/tiledb/cloud/vcf/__init__.py
@@ -1,11 +1,25 @@
 from .allele_frequency import read_allele_frequency
+from .ingestion import Contigs
 from .ingestion import ingest
 from .query import build_read_dag
 from .query import read
+from .utils import bgzip_and_index
+from .utils import create_index_file
+from .utils import find_index
+from .utils import get_record_count
+from .utils import get_sample_name
+from .utils import is_bgzipped
 
 __all__ = [
+    "Contigs",
     "ingest",
     "build_read_dag",
     "read",
     "read_allele_frequency",
+    "bgzip_and_index",
+    "create_index_file",
+    "find_index",
+    "get_sample_name",
+    "get_record_count",
+    "is_bgzipped",
 ]
