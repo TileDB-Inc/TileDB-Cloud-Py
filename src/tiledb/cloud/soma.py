1a3f2a553a9059de560ec37c5d66325939af8fb3
John Kerl <kerl.john.r@gmail.com>
2023-07-24 19:20:30 -0400

    more testing

--- src/tiledb/cloud/soma.py
+++ src/tiledb/cloud/soma.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import re
 from typing import ContextManager, Dict, Optional
 from unittest import mock
 
@@ -80,7 +82,7 @@ def ingest_h5ad(
     soma_ctx = tiledbsoma.SOMATileDBContext()
     if extra_tiledb_config:
         soma_ctx = soma_ctx.replace(tiledb_config=extra_tiledb_config)
-    with tiledb.VFS().open(input_uri) as input_file:
+    with tiledb.VFS(ctx=soma_ctx.tiledb_ctx).open(input_uri) as input_file:
         with _hack_patch_anndata_byval():
             input_data = anndata.read_h5ad(_FSPathWrapper(input_file, input_uri), "r")
         output_uri = io.from_anndata(
@@ -99,6 +101,7 @@ def run_ingest_workflow(
     output_uri: str,
     input_uri: str,
     measurement_name: str,
+    pattern: Optional[str] = None,
     extra_tiledb_config: Optional[Dict[str, object]] = None,
     platform_config: Optional[Dict[str, object]] = None,
     ingest_mode: str = "write",
@@ -111,12 +114,20 @@ def run_ingest_workflow(
     :param output_uri: The output URI to write to. This will probably look like
         ``tiledb://namespace/some://storage/uri``.
     :param input_uri: The URI of the H5AD file(s) to read from. These are read
-        using TileDB VFS, so any path supported (and accessible) will work.  If the ``input_uri``
-        passes ``vfs.is_file``, it's ingested.  If the ``input_uri`` passes ``vfs.is_dir``, then all
-        first-level entries are ingested .  In either case, an input file is skipped if ``pattern``
-        is provided and doesn't match the input file.
+        using TileDB VFS, so any path supported (and accessible) will work.  If the
+        ``input_uri`` passes ``vfs.is_file``, it's ingested.  If the ``input_uri``
+        passes ``vfs.is_dir``, then all first-level entries are ingested .  In the
+        latter, directory case, an input file is skipped if ``pattern`` is provided
+        and doesn't match the input file. As well, in the directory case, each entry's
+        basename is appended to the ``output_uri`` to form the entry's output URI.
+        For example, if ``a.h5ad` and ``b.h5ad`` are present within ``input_uri`` of
+        ``s3://bucket/h5ads/`` and ``output_uri`` is
+        `tiledb://namespace/s3://bucket/somas``, then
+        ``tiledb://namespace/s3://bucket/somas/a`` and
+        ``tiledb://namespace/s3://bucket/somas/b`` are written.
     :param measurement_name: The name of the Measurement within the Experiment
         to store the data.
+    :param pattern: As described for ``input_uri``.
     :param extra_tiledb_config: Extra configuration for TileDB.
     :param platform_config: The SOMA ``platform_config`` value to pass in,
         if any.
@@ -130,13 +141,11 @@ def run_ingest_workflow(
     :return: A dictionary of ``{"status": "started", "graph_id": ...}``,
         with the UUID of the graph on the server side, which can be used to
         manage execution and monitor progress.
-    :param pattern: As described for ``input_uri``.
     """
 
-    vfs = tiledb.VFS()
+    vfs = tiledb.VFS(config=extra_tiledb_config)
 
     if vfs.is_file(input_uri):
-        # XXX pattern-match check
         grf = dag.DAG(
             name="ingest-h5ad-file",
             mode=dag.Mode.BATCH,
@@ -167,17 +176,26 @@ def run_ingest_workflow(
         )
 
         collector = grf.submit(
-            _write_me,
+            lambda output_uri: output_uri,
             output_uri=output_uri,
-            input_uri=input_uri,
         )
 
-        for entry_uri in vfs.ls(input_uri):
-            # XXX pattern-match check
-            node = submitter(
+        for entry_input_uri in vfs.ls(input_uri):
+            base = os.path.basename(entry_input_uri)
+            base, _ = os.path.splitext(base)
+
+            entry_output_uri = output_uri + "/" + base
+            if not output_uri.endswith("/"):
+                entry_output_uri += "/"
+            entry_output_uri += base
+
+            if pattern is not None and not bool(re.match(pattern, entry_input_uri)):
+                continue
+
+            node = grf.submit(
                 _ingest_h5ad_byval,
-                output_uri=output_uri,
-                input_uri=input_uri,
+                output_uri=entry_output_uri,
+                input_uri=entry_input_uri,
                 measurement_name=measurement_name,
                 extra_tiledb_config=extra_tiledb_config,
                 ingest_mode=ingest_mode,
@@ -185,7 +203,7 @@ def run_ingest_workflow(
                 resources=_DEFAULT_RESOURCES if resources is None else resources,
                 access_credentials_name=access_credentials_name,
             )
-            parent.depends_on(node)
+            collector.depends_on(node)
 
         grf.compute()
         return {
@@ -195,8 +213,12 @@ def run_ingest_workflow(
 
     raise ValueError(f"input_uri {input_uri!r} is neither file nor directory")
 
-# Until we fully get this version of tiledb.cloud deployed server-side, we want
-# to refer to `ingest_h5ad` by value rather than by reference.
+
+# Until we fully get this version of tiledb.cloud deployed server-side, we must
+# refer to all functions by value rather than by reference -- which is a fancy way
+# of saying these functions _will not work at all_ until and unless they are
+# checked into tiledb-cloud-py and deployed server-side. _All_ dev work _must_
+# use this idiom.
 _ingest_h5ad_byval = functions.to_register_by_value(ingest_h5ad)
 _run_ingest_workflow = functions.to_register_by_value(run_ingest_workflow)
 _hack_patch_anndata_byval = functions.to_register_by_value(_hack_patch_anndata)
