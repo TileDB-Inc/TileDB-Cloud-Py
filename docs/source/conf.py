5c64bad47e7015c5c30ec1313376a1dee1c64a35
Paul Fisher <paul.fisher@tiledb.com>
2023-08-14 10:47:13 -0400

    Build documentation using existing Sphinx setup.
    This uses the infrastructure we have for Sphinx to generate and build
    a GitHub Pages website with our docs. Based on:
    
    https://coderefinery.github.io/documentation/gh_workflow/#exercise-deploy-sphinx-documentation-to-github-pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from typing import List
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
@@ -37,7 +38,7 @@ author = "TileDB, Inc."
 # ones.
 extensions = [
     # 'sphinx.ext.autodoc',
-    #'sphinxcontrib.apidoc',
+    # 'sphinxcontrib.apidoc',
     "sphinx.ext.autodoc",  # Core library for html generation from docstrings
     "sphinx.ext.autosummary",  # Create neat summary tables
 ]
@@ -59,7 +60,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_autosummary/tiledb.cloud.rest_api.test.*"]
+exclude_patterns: List[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
