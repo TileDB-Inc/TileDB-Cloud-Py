745228d4a6ef8d94b855232b1144cbb7ba96e8c9
John Kerl <kerl.john.r@gmail.com>
2023-12-07 11:40:51 -0500

    Expose alternate graph return value on ingest (#490)
    * top-down structure in soma/ingest.py
    
    * Expose alternate graph return value on ingest
    
    * code-review feedback

--- src/tiledb/cloud/soma/__init__.py
+++ src/tiledb/cloud/soma/__init__.py
@@ -1,3 +1,4 @@
+from .ingest import build_ingest_workflow_graph
 from .ingest import run_ingest_workflow
 
-__all__ = ["run_ingest_workflow"]
+__all__ = ["build_ingest_workflow_graph", "run_ingest_workflow"]
