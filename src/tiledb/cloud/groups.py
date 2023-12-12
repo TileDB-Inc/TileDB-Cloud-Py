dc90d7b8c1c98b69f74e138e7ce08fc0f98ca59e
Paris Morgan <jparismorgan@gmail.com>
2023-12-12 15:01:25 +0100

    raise error if no storage

--- src/tiledb/cloud/groups.py
+++ src/tiledb/cloud/groups.py
@@ -162,7 +162,8 @@ def _default_ns_path_cred(namespace: Optional[str] = None) -> Tuple[str, str, st
     if storage:
         path = storage.path
         cred_name = storage.credentials_name
-
+    if not path and not principal.default_s3_path:
+        raise ValueError(f"No storage provider configured.")
     path = path or (principal.default_s3_path + "/groups")
     cred_name = cred_name or principal.default_s3_path_credentials_name
     return namespace, path, cred_name
