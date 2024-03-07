c8026e778a5dc578c579fffc7b1585e847622203
jgmoutafis <jg2moutafis@gmail.com>
2024-03-07 12:58:03 +0200

    Add fetch invitation list method.

--- src/tiledb/cloud/invites.py
+++ src/tiledb/cloud/invites.py
@@ -25,6 +25,36 @@ def accept_invitation(invitation_id: str) -> None:
         raise tiledb_cloud_error.check_exc(exc)
 
 
+def fetch_invitations(**filters):
+    """
+    Fetches a paginated list of invitations.
+
+    Args (filters):
+        organization (str): name or ID of organization to filter
+        array (str): name/uri of array that is url-encoded to filter
+        group (str): name or ID of group to filter
+        start (int): start time for tasks to filter by
+        end (int): end time for tasks to filter by
+        page (int): pagination offset
+        per_page (int): pagination limit
+        type (str): invitation type, \"ARRAY_SHARE\", \"JOIN_ORGANIZATION\"
+        status (str): Filter to only return \"PENDING\", \"ACCEPTED\"
+        orderby (str): sort by which field valid values include
+                       timestamp, array_name, organization_name
+
+    Raises:
+        TileDBCloudError
+
+    Returns:
+        dict: Invitations and pagination metadata.
+    """
+    invitation_api = client.build(rest_api.InvitationApi)
+    try:
+        return invitation_api.fetch_invitations(**filters)
+    except rest_api.ApiException as exc:
+        raise tiledb_cloud_error.check_exc(exc)
+
+
 def invite_to_organization(organization: str, email_invite: List[str]) -> None:
     """
     Sends email to multiple recipients with joining information
