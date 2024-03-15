74600bd236d45ef3831333a64c8f30ca3077718f
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2024-03-15 13:21:01 +0200

    Add support for OpenAI embeddings and consolidate directory listing args

--- src/tiledb/cloud/vector_search/file_ingestion.py
+++ src/tiledb/cloud/vector_search/file_ingestion.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Sequence
+from typing import Dict, List, Mapping, Optional, Sequence
 
 from tiledb.cloud import dag
 from tiledb.cloud.utilities import as_batch
@@ -10,6 +10,7 @@ def ingest_files_dag(
     file_name: Optional[str] = None,
     acn: Optional[str] = None,
     config=None,
+    environment_variables: Mapping[str, str] = {},
     namespace: Optional[str] = None,
     verbose: bool = False,
     trace_id: Optional[str] = None,
@@ -18,24 +19,33 @@ def ingest_files_dag(
     index_type: str = "IVF_FLAT",
     index_creation_kwargs: Dict = {},
     # DirectoryTextReader params
-    glob: str = "**/[!.]*",
-    exclude: Sequence[str] = (),
+    include: str = "*",
+    exclude: Sequence[str] = ["[.]*", "*/[.]*"],
     suffixes: Optional[Sequence[str]] = None,
+    max_files: Optional[int] = None,
     text_splitter: str = "RecursiveCharacterTextSplitter",
-    text_splitter_kwargs: Optional[Dict] = {
+    text_splitter_kwargs: Dict = {
         "chunk_size": 500,
         "chunk_overlap": 50,
     },
-    # SentenceTransformersEmbedding params
-    model_name_or_path: str = "BAAI/bge-small-en-v1.5",
+    # Embedding params
+    embedding_class: str = "LangChainEmbedding",
+    embedding_kwargs: Dict = {
+        "dimensions": 1536,
+        "embedding_class": "OpenAIEmbeddings",
+        "embedding_kwargs": {
+            "model": "text-embedding-ada-002",
+        },
+    },
+    openai_key: Optional[str] = None,
     # Index update params
-    index_timestamp: int = None,
+    index_timestamp: Optional[int] = None,
     workers: int = -1,
-    worker_resources: Dict = None,
-    worker_image: str = None,
+    worker_resources: Optional[Dict] = None,
+    worker_image: Optional[str] = None,
     extra_worker_modules: Optional[List[str]] = None,
     driver_resources: Dict = {"cpu": "2", "memory": "8Gi"},
-    driver_image: str = None,
+    driver_image: Optional[str] = None,
     extra_driver_modules: Optional[List[str]] = None,
     max_tasks_per_stage: int = -1,
     embeddings_generation_mode: dag.Mode = dag.Mode.LOCAL,
@@ -61,11 +71,14 @@ def ingest_files_dag(
     :param index_type: Vector search index type ("FLAT", "IVF_FLAT").
     :param index_creation_kwargs: Arguments to be passed to the index creation method
     # DirectoryTextReader params.
-    :param glob: Glob pattern relative to the specified path by default set to pick up
-        all non-hidden files.
-    :param exclude: Patterns to exclude from results, use glob syntax.
-    :param suffixes: Load only files with these suffixes. Suffixes must include the
-        dot, e.g. ".txt".
+    :param include: File pattern to iclude relative to `file_dir_uri`. By default set
+        to include all files.
+    :param exclude: File patterns to exclude relative to `file_dir_uri`. By default
+        set to ignore all hidden files.
+    :param suffixes: Provide to keep only files with these suffixes
+        Useful when wanting to keep files with different suffixes
+        Suffixes must include the dot, e.g. ".txt"
+    :param max_files: Maximum number of files to include.
     :param text_splitter_kwargs: Arguments for the splitter class.
     # SentenceTransformersEmbedding params.
     :param model_name_or_path: Huggingface SentenceTransformer model name or path
@@ -100,32 +113,42 @@ def ingest_files_dag(
         file_name: Optional[str] = None,
         acn: Optional[str] = None,
         config=None,
+        environment_variables: Mapping[str, str] = {},
         namespace: Optional[str] = None,
         verbose: bool = False,
         trace_id: Optional[str] = None,
         # Index creation params
-        create_index: bool = True,
+        create_index: bool = False,
         index_type: str = "IVF_FLAT",
         index_creation_kwargs: Dict = {},
         # DirectoryTextReader params
-        glob: str = "**/[!.]*",
-        exclude: Sequence[str] = (),
+        include: str = "*",
+        exclude: Sequence[str] = ["[.]*", "*/[.]*"],
         suffixes: Optional[Sequence[str]] = None,
+        max_files: Optional[int] = None,
         text_splitter: str = "RecursiveCharacterTextSplitter",
-        text_splitter_kwargs: Optional[Dict] = {
+        text_splitter_kwargs: Dict = {
             "chunk_size": 500,
             "chunk_overlap": 50,
         },
-        # SentenceTransformersEmbedding params
-        model_name_or_path: str = "BAAI/bge-small-en-v1.5",
+        # Embedding params
+        embedding_class: str = "LangChainEmbedding",
+        embedding_kwargs: Dict = {
+            "dimensions": 1536,
+            "embedding_class": "OpenAIEmbeddings",
+            "embedding_kwargs": {
+                "model": "text-embedding-ada-002",
+            },
+        },
+        openai_key: Optional[str] = None,
         # Index update params
-        index_timestamp: int = None,
+        index_timestamp: Optional[int] = None,
         workers: int = -1,
-        worker_resources: Dict = None,
-        worker_image: str = None,
+        worker_resources: Optional[Dict] = None,
+        worker_image: Optional[str] = None,
         extra_worker_modules: Optional[List[str]] = None,
         driver_resources: Dict = {"cpu": "2", "memory": "8Gi"},
-        driver_image: str = None,
+        driver_image: Optional[str] = None,
         extra_driver_modules: Optional[List[str]] = None,
         max_tasks_per_stage: int = -1,
         embeddings_generation_mode: dag.Mode = dag.Mode.LOCAL,
@@ -143,6 +166,7 @@ def ingest_files_dag(
         :param acn: Access Credentials Name (ACN) registered in TileDB Cloud (ARN type),
             defaults to None.
         :param config: config dictionary, defaults to None.
+        :param environment_variables: Environment variables to use during ingestion.
         :param namespace: TileDB-Cloud namespace, defaults to None.
         :param verbose: verbose logging, defaults to False.
         :param trace_id: trace ID for logging, defaults to None.
@@ -152,11 +176,14 @@ def ingest_files_dag(
         :param index_creation_kwargs: Arguments to be passed to the index creation
             method.
         # DirectoryTextReader params.
-        :param glob: Glob pattern relative to the specified path by default set to
-            pick up all non-hidden files.
-        :param exclude: Patterns to exclude from results, use glob syntax.
-        :param suffixes: Load only files with these suffixes. Suffixes must include
-            the dot, e.g. ".txt".
+        :param include: File pattern to iclude relative to `file_dir_uri`. By default
+            set to include all files.
+        :param exclude: File patterns to exclude relative to `file_dir_uri`. By default
+            set to ignore all hidden files.
+        :param suffixes: Provide to keep only files with these suffixes
+            Useful when wanting to keep files with different suffixes
+            Suffixes must include the dot, e.g. ".txt"
+        :param max_files: Maximum number of files to include.
         :param text_splitter_kwargs: Arguments for the splitter class.
         # SentenceTransformersEmbedding params.
         :param model_name_or_path: Huggingface SentenceTransformer model name or path
@@ -184,8 +211,9 @@ def ingest_files_dag(
         :param vector_indexing_mode: TaskGraph execution mode for the vector indexing.
         :param index_update_kwargs: Extra arguments to pass to the index update job.
         """
+        import importlib
+
         import tiledb
-        from tiledb.vector_search.embeddings import SentenceTransformersEmbedding
         from tiledb.vector_search.object_api import object_index
         from tiledb.vector_search.object_readers import DirectoryTextReader
 
@@ -198,16 +226,20 @@ def ingest_files_dag(
 
         reader = DirectoryTextReader(
             uri=file_dir_uri,
-            glob=f"**/{file_name}" if file_name is not None else glob,
+            include=f"{file_name}" if file_name is not None else include,
             exclude=exclude,
             suffixes=suffixes,
+            max_files=max_files,
             text_splitter=text_splitter,
             text_splitter_kwargs=text_splitter_kwargs,
         )
 
-        embedding = SentenceTransformersEmbedding(
-            model_name_or_path=model_name_or_path,
-        )
+        if openai_key is not None:
+            environment_variables["OPENAI_API_KEY"] = openai_key
+        embeddings_module = importlib.import_module("tiledb.vector_search.embeddings")
+        embedding_class_ = getattr(embeddings_module, embedding_class)
+        embedding = embedding_class_(**embedding_kwargs)
+
         index_uri_exists = index_exists(
             index_uri=index_uri,
             config=config,
@@ -224,12 +256,16 @@ def ingest_files_dag(
                     object_reader=reader,
                     embedding=embedding,
                     config=config,
+                    environment_variables=environment_variables,
                     **index_creation_kwargs,
                 )
         else:
             if index_uri_exists:
                 index = object_index.ObjectIndex(
-                    uri=index_uri, load_metadata_in_memory=False, memory_budget=1
+                    uri=index_uri,
+                    environment_variables=environment_variables,
+                    load_metadata_in_memory=False,
+                    memory_budget=1,
                 )
             else:
                 index = object_index.create(
@@ -238,6 +274,7 @@ def ingest_files_dag(
                     object_reader=reader,
                     embedding=embedding,
                     config=config,
+                    environment_variables=environment_variables,
                     **index_creation_kwargs,
                 )
 
@@ -256,6 +293,7 @@ def ingest_files_dag(
             embeddings_generation_driver_mode=embeddings_generation_driver_mode,
             vector_indexing_mode=vector_indexing_mode,
             config=config,
+            environment_variables=environment_variables,
             namespace=namespace,
             verbose=verbose,
             trace_id=trace_id,
@@ -278,6 +316,7 @@ def ingest_files_dag(
         image_name="vectorsearch",
         acn=acn,
         config=config,
+        environment_variables=environment_variables,
         namespace=namespace,
         verbose=verbose,
         trace_id=trace_id,
@@ -285,12 +324,15 @@ def ingest_files_dag(
         index_type=index_type,
         index_creation_kwargs=index_creation_kwargs,
         file_name=file_name,
-        glob=glob,
+        include=include,
         exclude=exclude,
         suffixes=suffixes,
+        max_files=max_files,
         text_splitter=text_splitter,
         text_splitter_kwargs=text_splitter_kwargs,
-        model_name_or_path=model_name_or_path,
+        embedding_class=embedding_class,
+        embedding_kwargs=embedding_class,
+        openai_key=openai_key,
         index_timestamp=index_timestamp,
         workers=workers,
         worker_resources=worker_resources,
