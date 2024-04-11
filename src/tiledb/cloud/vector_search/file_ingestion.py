8803f139c3b07f1781ccd4f26f9ab40dc690b766
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2024-04-11 14:30:13 +0300

    Apply code restructure suggestion

--- src/tiledb/cloud/vector_search/file_ingestion.py
+++ src/tiledb/cloud/vector_search/file_ingestion.py
@@ -4,7 +4,7 @@ from tiledb.cloud import dag
 from tiledb.cloud.utilities import as_batch
 
 
-def ingest_files_udf(
+def ingest_files(
     file_dir_uri: str,
     index_uri: str,
     file_name: Optional[str] = None,
@@ -57,7 +57,7 @@ def ingest_files_udf(
     :param namespace: TileDB-Cloud namespace, defaults to None.
     :param verbose: verbose logging, defaults to False.
     :param trace_id: trace ID for logging, defaults to None.
-    # Index creation params
+    # Vector Index params
     :param index_type: Vector search index type ("FLAT", "IVF_FLAT").
     :param index_creation_kwargs: Arguments to be passed to the index creation
         method.
@@ -97,88 +97,259 @@ def ingest_files_udf(
     :param vector_indexing_mode: TaskGraph execution mode for the vector indexing.
     :param index_update_kwargs: Extra arguments to pass to the index update job.
     """
-    import tiledb
-    import tiledb.vector_search.embeddings as embeddings_module
-    from tiledb.vector_search.object_api import object_index
-    from tiledb.vector_search.object_readers import DirectoryTextReader
+    import logging
 
-    if environment_variables is None:
-        environment_variables = {}
-    if index_creation_kwargs is None:
-        index_creation_kwargs = {}
-    if index_update_kwargs is None:
-        index_update_kwargs = {"files_per_partition": 100}
-    if embedding_kwargs is None:
-        embedding_kwargs = {
-            "dimensions": 1536,
-            "embedding_class": "OpenAIEmbeddings",
-            "embedding_kwargs": {
-                "model": "text-embedding-ada-002",
-            },
-        }
-    if text_splitter_kwargs is None:
-        text_splitter_kwargs = {
-            "chunk_size": 500,
-            "chunk_overlap": 50,
-        }
-    if driver_resources is None:
+    import tiledb
+    from tiledb.cloud import dag
+    from tiledb.cloud.utilities import get_logger
+    from tiledb.cloud.utilities import run_dag
+
+    DEFAULT_IMG_NAME = "vectorsearch"
+
+    def get_logger_wrapper(
+        verbose: bool = False,
+    ) -> logging.Logger:
+        """
+        Get a logger instance and log version information.
+
+        :param verbose: verbose logging, defaults to False
+        :return: logger instance
+        """
+
+        level = logging.DEBUG if verbose else logging.INFO
+        logger = get_logger(level)
+
+        logger.debug(
+            "tiledb.cloud=%s, tiledb=%s, libtiledb=%s",
+            tiledb.cloud.version.version,
+            tiledb.version(),
+            tiledb.libtiledb.version(),
+        )
+
+        return logger
+
+    # --------------------------------------------------------------------
+    # UDFs
+    # --------------------------------------------------------------------
+
+    def create_dataset_udf(
+        file_dir_uri: str,
+        index_uri: str,
+        file_name: Optional[str] = None,
+        config=None,
+        environment_variables: Optional[Mapping[str, str]] = None,
+        verbose: bool = False,
+        # Index creation params
+        index_type: str = "IVF_FLAT",
+        index_creation_kwargs: Optional[Dict] = None,
+        # DirectoryTextReader params
+        include: str = "*",
+        exclude: Optional[Sequence[str]] = ("[.]*", "*/[.]*"),
+        suffixes: Optional[Sequence[str]] = None,
+        max_files: Optional[int] = None,
+        text_splitter: str = "RecursiveCharacterTextSplitter",
+        text_splitter_kwargs: Optional[Dict] = None,
+        # Embedding params
+        embedding_class: str = "LangChainEmbedding",
+        embedding_kwargs: Optional[Dict] = None,
+    ) -> str:
+        """
+        Create a TileDB vector search dataset.
+        """
+        import tiledb.vector_search as vs
+        import tiledb.vector_search.embeddings as embeddings_module
+        from tiledb.vector_search.object_api import object_index
+        from tiledb.vector_search.object_readers import DirectoryTextReader
+
+        logger = get_logger_wrapper(verbose)
+        logger.debug("tiledb-vector-search=%s", vs.__version__)
+
+        # Check if the dataset already exists
+        with tiledb.scope_ctx(config):
+            if environment_variables is None:
+                environment_variables = {}
+            if index_creation_kwargs is None:
+                index_creation_kwargs = {}
+            if embedding_kwargs is None:
+                embedding_kwargs = {
+                    "dimensions": 1536,
+                    "embedding_class": "OpenAIEmbeddings",
+                    "embedding_kwargs": {
+                        "model": "text-embedding-ada-002",
+                    },
+                }
+            if text_splitter_kwargs is None:
+                text_splitter_kwargs = {
+                    "chunk_size": 500,
+                    "chunk_overlap": 50,
+                }
+
+            reader = DirectoryTextReader(
+                search_uri=file_dir_uri,
+                include=file_name if file_name is not None else include,
+                exclude=exclude,
+                suffixes=suffixes,
+                max_files=max_files,
+                text_splitter=text_splitter,
+                text_splitter_kwargs=text_splitter_kwargs,
+            )
+
+            embedding_class_ = getattr(embeddings_module, embedding_class)
+            embedding = embedding_class_(**embedding_kwargs)
+
+            if tiledb.object_type(index_uri) == "group":
+                logger.info("Existing dataset: %r. Updating reader.", index_uri)
+                index = object_index.ObjectIndex(
+                    uri=index_uri,
+                    environment_variables=environment_variables,
+                    load_embedding=False,
+                    load_metadata_in_memory=False,
+                    memory_budget=1,
+                )
+                index.update_object_reader(reader)
+                return index_uri
+            else:
+                logger.info("Creating dataset: %r", index_uri)
+                object_index.create(
+                    uri=index_uri,
+                    index_type=index_type,
+                    object_reader=reader,
+                    embedding=embedding,
+                    config=config,
+                    environment_variables=environment_variables,
+                    **index_creation_kwargs,
+                )
+                return index_uri
+
+    def ingest_files_udf(
+        index_uri: str,
+        acn: Optional[str] = None,
+        config=None,
+        environment_variables: Optional[Mapping[str, str]] = None,
+        openai_key: Optional[str] = None,
+        namespace: Optional[str] = None,
+        verbose: bool = False,
+        trace_id: Optional[str] = None,
+        # Index update params
+        index_timestamp: Optional[int] = None,
+        workers: int = -1,
+        worker_resources: Optional[Dict] = None,
+        worker_image: Optional[str] = None,
+        extra_worker_modules: Optional[List[str]] = None,
+        driver_resources: Optional[Dict] = None,
+        driver_image: Optional[str] = None,
+        extra_driver_modules: Optional[List[str]] = None,
+        max_tasks_per_stage: int = -1,
+        embeddings_generation_mode: dag.Mode = dag.Mode.LOCAL,
+        embeddings_generation_driver_mode: dag.Mode = dag.Mode.LOCAL,
+        vector_indexing_mode: dag.Mode = dag.Mode.LOCAL,
+        index_update_kwargs: Optional[Dict] = None,
+    ):
+        """
+        Ingest files into a vector search text index.
+        """
+        from tiledb.vector_search.object_api import object_index
+
+        if environment_variables is None:
+            environment_variables = {}
+        if openai_key is not None:
+            environment_variables["OPENAI_API_KEY"] = openai_key
+        if index_update_kwargs is None:
+            index_update_kwargs = {"files_per_partition": 100}
+
+        index = object_index.ObjectIndex(
+            uri=index_uri,
+            environment_variables=environment_variables,
+            load_metadata_in_memory=False,
+            memory_budget=1,
+        )
+        index.update_index(
+            index_timestamp=index_timestamp,
+            workers=workers,
+            worker_resources=worker_resources,
+            worker_image=worker_image,
+            extra_worker_modules=extra_worker_modules,
+            driver_resources=driver_resources,
+            driver_image=driver_image,
+            extra_driver_modules=extra_driver_modules,
+            worker_access_credentials_name=acn,
+            max_tasks_per_stage=max_tasks_per_stage,
+            embeddings_generation_mode=embeddings_generation_mode,
+            embeddings_generation_driver_mode=embeddings_generation_driver_mode,
+            vector_indexing_mode=vector_indexing_mode,
+            config=config,
+            environment_variables=environment_variables,
+            namespace=namespace,
+            verbose=verbose,
+            trace_id=trace_id,
+            **index_update_kwargs,
+        )
+
+    # --------------------------------------------------------------------
+    # DAG
+    # --------------------------------------------------------------------
+
+    graph = dag.DAG(
+        name="vector-search-file-indexing",
+        namespace=namespace,
+        mode=dag.Mode.BATCH,
+    )
+    if worker_resources is None:
         driver_resources = {"cpu": "2", "memory": "8Gi"}
 
-    reader = DirectoryTextReader(
-        search_uri=file_dir_uri,
-        include=file_name if file_name is not None else include,
+    create_index_node = graph.submit(
+        create_dataset_udf,
+        file_dir_uri=file_dir_uri,
+        index_uri=index_uri,
+        file_name=file_name,
+        config=config,
+        environment_variables=environment_variables,
+        verbose=verbose,
+        index_type=index_type,
+        index_creation_kwargs=index_creation_kwargs,
+        include=include,
         exclude=exclude,
         suffixes=suffixes,
         max_files=max_files,
         text_splitter=text_splitter,
         text_splitter_kwargs=text_splitter_kwargs,
+        embedding_class=embedding_class,
+        embedding_kwargs=embedding_kwargs,
+        name="Create Vector Search index",
+        access_credentials_name=acn,
+        resources={"cpu": "1", "memory": "2Gi"},
+        image_name=DEFAULT_IMG_NAME,
     )
 
-    if openai_key is not None:
-        environment_variables["OPENAI_API_KEY"] = openai_key
-    embedding_class_ = getattr(embeddings_module, embedding_class)
-    embedding = embedding_class_(**embedding_kwargs)
-
-    with tiledb.scope_ctx(config):
-        if tiledb.object_type(index_uri) == "group":
-            index = object_index.ObjectIndex(
-                uri=index_uri,
-                environment_variables=environment_variables,
-                load_metadata_in_memory=False,
-                memory_budget=1,
-            )
-        else:
-            index = object_index.create(
-                uri=index_uri,
-                index_type=index_type,
-                object_reader=reader,
-                embedding=embedding,
-                config=config,
-                environment_variables=environment_variables,
-                **index_creation_kwargs,
-            )
-
-    index.update_index(
-        index_timestamp=index_timestamp,
+    ingest_files_node = graph.submit(
+        ingest_files_udf,
+        index_uri=index_uri,
+        acn=acn,
+        config=config,
+        environment_variables=environment_variables,
+        openai_key=openai_key,
+        namespace=namespace,
+        verbose=verbose,
+        trace_id=trace_id,
         workers=workers,
-        worker_resources=worker_resources,
         worker_image=worker_image,
         extra_worker_modules=extra_worker_modules,
         driver_resources=driver_resources,
         driver_image=driver_image,
         extra_driver_modules=extra_driver_modules,
-        worker_access_credentials_name=acn,
         max_tasks_per_stage=max_tasks_per_stage,
         embeddings_generation_mode=embeddings_generation_mode,
         embeddings_generation_driver_mode=embeddings_generation_driver_mode,
         vector_indexing_mode=vector_indexing_mode,
-        config=config,
-        environment_variables=environment_variables,
-        namespace=namespace,
-        verbose=verbose,
-        trace_id=trace_id,
-        **index_update_kwargs,
+        index_update_kwargs=index_update_kwargs,
+        name="Ingest Files to Vector Search index",
+        access_credentials_name=acn,
+        resources=worker_resources,
+        image_name=DEFAULT_IMG_NAME,
     )
 
+    ingest_files_node.depends_on(create_index_node)
+    run_dag(graph, debug=verbose)
 
-ingest = as_batch(ingest_files_udf)
+
+ingest = as_batch(ingest_files)
