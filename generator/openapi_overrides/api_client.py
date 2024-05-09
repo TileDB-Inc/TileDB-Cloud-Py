8c53d90cf9eacb0929bb217647a4571efcdf5306
Paul Fisher <paul.fisher@tiledb.com>
2024-05-09 16:26:45 -0400

    Update code generation to support v2 API returns.
    When returning data from an API, the generated OpenAPI code looks it up
    by name. This meant that, since we needed to use the same API client
    types across both the v1 and v2 APIs, v2 API returns would attempt to
    look up their named types in `tiledb.cloud.rest_api.models`, where they
    would not exist.
    
    We managed to get away with this until something in the v2 API changed
    and was defined as returning an object instead of None.

--- generator/openapi_overrides/api_client.py
+++ generator/openapi_overrides/api_client.py
@@ -1,3 +1,12 @@
 """Re-exporting the ``api_client`` namespace."""
 
 from tiledb.cloud.rest_api.api_client import *  # noqa: F401,F403
+
+_ApiClientCls = ApiClient  # noqa: F405
+
+
+def ApiClient(*args, **kwargs) -> _ApiClientCls:
+    """Returns an ``ApiClient`` suitable for the v2 API models."""
+    from . import models
+
+    return _ApiClientCls(*args, **kwargs, _tdb_models_module=models)
