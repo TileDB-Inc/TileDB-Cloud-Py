400a1f2fc388a3a800a07a7355be1c610aa688ba
Sean Gillies <sean.gillies@tiledb.com>
2024-05-22 14:38:28 -0600

    Add delete()
    Remove config for an uninstalled pytest plugin

--- src/tiledb/cloud/asset.py
+++ src/tiledb/cloud/asset.py
@@ -1,8 +1,8 @@
 """An asset may be an array or a group."""
 
-import tiledb.cloud  # type: ignore
+from functools import partial
 
-asset_type_map = {"array": tiledb.cloud.array, "group": tiledb.cloud.groups}
+import tiledb.cloud  # type: ignore
 
 
 def info(uri: str) -> object:
@@ -13,6 +13,22 @@ def info(uri: str) -> object:
     """
     # Note: the URI can be either of the two forms, yes?
     # tiledb://namespace/name or tiledb://namespace/UUID.
+    info_map = {"array": tiledb.cloud.array.info, "group": tiledb.cloud.groups.info}
     ctx = tiledb.cloud.Ctx()
-    mod = asset_type_map[tiledb.object_type(uri, ctx=ctx)]
-    return getattr(mod, "info")(uri)
+    func = info_map[tiledb.object_type(uri, ctx=ctx)]
+    return func(uri)
+
+
+def delete(uri: str, recursive: bool = False) -> None:
+    """Deregister the asset and remove its physical groups and arrays from storage.
+
+    :param uri: URI of the asset.
+    :return: None.
+    """
+    delete_map = {
+        "array": tiledb.cloud.array.delete_array,
+        "group": partial(tiledb.cloud.groups.delete, recursive=recursive),
+    }
+    ctx = tiledb.cloud.Ctx()
+    func = delete_map[tiledb.object_type(uri, ctx=ctx)]
+    return func(uri)
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -2,9 +2,6 @@
 
 from unittest import mock
 
-import tiledb
-import tiledb.cloud
-
 from tiledb.cloud import asset
 
 
@@ -16,6 +13,7 @@ def test_asset_info_array(array_info, object_type):
     array_info.return_value = {"tiledb_uri": "tiledb://a"}
 
     assert asset.info("a") == {"tiledb_uri": "tiledb://a"}
+    array_info.assert_called_once_with("a")
 
 
 @mock.patch("tiledb.object_type")
@@ -26,6 +24,7 @@ def test_asset_info_group(groups_info, object_type):
     groups_info.return_value = {"tiledb_uri": "tiledb://g"}
 
     assert asset.info("g") == {"tiledb_uri": "tiledb://g"}
+    groups_info.assert_called_once_with("g")
 
 
 def test_prod_group_asset_info():
@@ -40,3 +39,33 @@ def test_prod_array_asset_info():
     info = asset.info("tiledb://TileDB-Inc/886afe8c-c055-4569-9644-351d8e72f098")
     assert info.name == "l_0.tdb"
     assert info.type == "dense"
+
+
+@mock.patch("tiledb.object_type")
+@mock.patch("tiledb.cloud.array.delete_array")
+def test_asset_delete_array(delete_array, object_type):
+    """Dispatch to array.array_delete when URI is an array."""
+    object_type.return_value = "array"
+
+    asset.delete("a")
+    delete_array.assert_called_once_with("a")
+
+
+@mock.patch("tiledb.object_type")
+@mock.patch("tiledb.cloud.groups.delete")
+def test_asset_delete_group(delete, object_type):
+    """Dispatch to groups.delete when URI is a group."""
+    object_type.return_value = "group"
+
+    asset.delete("g")
+    delete.assert_called_once_with("g", recursive=False)
+
+
+@mock.patch("tiledb.object_type")
+@mock.patch("tiledb.cloud.groups.delete")
+def test_asset_delete_group_recursive(delete, object_type):
+    """Dispatch to groups.delete when URI is a group."""
+    object_type.return_value = "group"
+
+    asset.delete("g", recursive=True)
+    delete.assert_called_once_with("g", recursive=True)
