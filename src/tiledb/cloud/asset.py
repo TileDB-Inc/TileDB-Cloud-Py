1934804035ed42b3c60816bace263eec863667a0
Sean Gillies <sean.gillies@tiledb.com>
2024-06-05 17:29:56 -0600

    Add register/deregister and update_info

--- src/tiledb/cloud/asset.py
+++ src/tiledb/cloud/asset.py
@@ -1,7 +1,7 @@
 """An asset may be an array or a group."""
 
 from functools import partial
-from typing import Callable, Mapping, Union
+from typing import Callable, Mapping, Optional, Union
 
 import tiledb  # type: ignore
 
@@ -11,7 +11,83 @@ from .rest_api.models import ArrayInfo  # type: ignore
 from .rest_api.models import GroupInfo  # type: ignore
 
 
-def delete(uri: str, recursive: bool = False) -> None:
+def register(
+    storage_uri: str,
+    type: str,
+    *,
+    name: Optional[str] = None,
+    namespace: Optional[str] = None,
+    credentials_name: Optional[str] = None,
+    parent_uri: Optional[str] = None,
+) -> None:
+    """
+    Register stored objects as an asset.
+
+    :param storage_uri: S3, for example, URI of the data to be registered.
+    :type uri: str
+    :param type: The type of asset, "array" or "group".
+    :type type: str
+    :param namespace: The user or organization to register the asset
+        under. If unset will default to the logged-in user's namespace.
+    :type namespace: str
+    :param name: Name of asset.
+    :type name: str
+    :param description: Optional description.
+    :type description: str
+    :param credentials_name: Optional name of access credentials to use.
+        If omitted, the default for namespace will be used.
+    :type credentials_name: str
+    :param parent_uri: Optional parent URI for group type assets.
+    :type parent_uri: str
+
+    For example, to register an array stored on S3 in a bucket named
+    'your-bucket' with a prefix 'prefix' to your own namespace:
+
+    >>> from tiledb.cloud import asset
+    >>> asset.register(
+    ...     "s3://your-bucket/prefix",
+    ...     "array",
+    ...     name="new-array",
+    ... )
+    """
+    if type == "array":
+        array.register_array(
+            storage_uri,
+            namespace=namespace,
+            array_name=name,
+            access_credentials_name=credentials_name,
+        )
+    elif type == "group":
+        groups.register(
+            storage_uri,
+            name=name,
+            namespace=namespace,
+            credentials_name=credentials_name,
+            parent_uri=parent_uri,
+        )
+    else:
+        raise tiledb.TileDBError(f"Invalid asset type '{type}'")
+
+
+def deregister(uri: str, *, recursive: Optional[bool] = False) -> None:
+    """Deregister an asset.
+
+    :param uri: tiledb URI of the asset.
+    :type uri: str
+    :param recursive: if True, contents of an asset will be recursively
+        deregistered Default: False.
+    :type recursive: bool
+    """
+    deregister_map: Mapping[str, Callable] = {
+        "array": array.deregister_array,
+        "group": partial(groups.deregister, recursive=recursive),
+    }
+    asset_type: str = tiledb.object_type(uri, ctx=tiledb.cloud.Ctx())
+    func = deregister_map[asset_type]
+    return func(uri)
+
+
+def delete(uri: str, *, recursive: Optional[bool] = False) -> None:
     """Deregister an asset and remove its objects from storage.
 
     :param uri: tiledb URI of the asset.
@@ -44,6 +120,51 @@ def info(uri: str) -> Union[ArrayInfo, GroupInfo]:
     return func(uri)
 
 
+def update_info(
+    uri: str,
+    *,
+    description: Optional[str] = None,
+    name: Optional[str] = None,
+    tags: Optional[list[str]] = None,
+    access_credentials_name: Optional[str] = None,
+) -> None:
+    """
+    Update asset info settings.
+
+    :param uri: tiledb URI of the asset.
+    :type uri: str
+    :param description: Asset description, defaults to None
+    :type description: str
+    :param name: Asset name, defaults to None
+    :type name: str
+    :param tags: Asset tags, defaults to None
+    :type tags: list[str]
+
+    For example, to update only the description of an asset, call update_info()
+    with only a 'description' keyword argument:
+
+    >>> from tiledb.cloud import asset
+    >>> asset.update_info(
+    ...     "tiledb://your-namespace/your-asset",
+    ...     description="This is a new description"
+    ... )
+    """
+    # Note: logo not yet supported.
+    asset_type: str = tiledb.object_type(uri, ctx=tiledb.cloud.Ctx())
+    if asset_type == "array":
+        return array.update_info(
+            uri,
+            array_name=name,
+            description=description,
+            access_credentials_name=access_credentials_name,
+            tags=tags,
+        )
+    elif asset_type == "group":
+        return groups.update_info(uri, description=description, name=name, tags=tags)
+    else:
+        raise tiledb.TileDBError(f"Invalid asset type '{asset_type}'")
+
+
 def list_shared_with(uri: str) -> None:
     """List an asset's sharing policies.
 
@@ -61,7 +182,7 @@ def list_shared_with(uri: str) -> None:
 
 
 def share(
-    uri: str, namespace: str, permissions: Union[str, list[str]] = "read"
+    uri: str, namespace: str, permissions: Optional[Union[str, list[str]]] = "read"
 ) -> None:
     """Give another namespace permission to access an asset.
 
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -1,12 +1,20 @@
 """Tests of the tiledb.cloud.asset module."""
 
+import time
+import unittest
 from unittest import mock
 
 from tiledb.cloud import asset  # type: ignore
+from tiledb.cloud import client
+from tiledb.cloud import groups
+from tiledb.cloud import rest_api
+from tiledb.cloud._common import testonly
 from tiledb.cloud.rest_api.models import ArrayInfo  # type: ignore
 from tiledb.cloud.rest_api.models import ArraySharing  # type: ignore
 from tiledb.cloud.rest_api.models import GroupInfo  # type: ignore
 from tiledb.cloud.rest_api.models import GroupSharing  # type: ignore
+
+TRIES = 5
 
 
 @mock.patch("tiledb.object_type", return_value="array")
@@ -117,7 +125,7 @@ def test_public_array_sharing():
         "tiledb://TileDB-Inc/cd89a0d6-c262-4729-9e75-d942879e1d7d"
     )
     assert len(sharing) == 2
-    assert sharing[0].namespace == "public"
+    assert "public" in set(policy.namespace for policy in sharing)
 
 
 def test_public_group_sharing():
@@ -127,3 +135,129 @@ def test_public_group_sharing():
     )
     assert len(sharing) == 1
     assert sharing[0].namespace == "public"
+
+
+@mock.patch("tiledb.object_type", return_value="array")
+@mock.patch("tiledb.cloud.array.update_info")
+def test_asset_update_info_array_dispatch(update_array, object_type):
+    """Dispatch to array.update_info when URI is an array."""
+    asset.update_info("a", description="new description")
+    update_array.assert_called_once_with(
+        "a",
+        array_name=None,
+        description="new description",
+        access_credentials_name=None,
+        tags=None,
+    )
+
+
+@mock.patch("tiledb.object_type", return_value="group")
+@mock.patch("tiledb.cloud.groups.update_info")
+def test_asset_update_info_group_dispatch(update_group, object_type):
+    """Dispatch to groups.update_info when URI is a group."""
+    asset.update_info("g", description="new description")
+    update_group.assert_called_once_with(
+        "g", name=None, description="new description", tags=None
+    )
+
+
+@mock.patch("tiledb.object_type", return_value="array")
+@mock.patch("tiledb.cloud.array.register_array")
+def test_asset_register_array_dispatch(register_array, object_type):
+    """Dispatch to array.register_array when URI is an array."""
+    asset.register("a", "array", name="foo", credentials_name="bar")
+    register_array.assert_called_once_with(
+        "a", namespace=None, array_name="foo", access_credentials_name="bar"
+    )
+
+
+@mock.patch("tiledb.object_type", return_value="group")
+@mock.patch("tiledb.cloud.groups.register")
+def test_asset_register_group_dispatch(register_group, object_type):
+    """Dispatch to groups.register when URI is a group."""
+    asset.register("a", "group", name="foo", credentials_name="bar")
+    register_group.assert_called_once_with(
+        "a", name="foo", namespace=None, credentials_name="bar", parent_uri=None
+    )
+
+
+@mock.patch("tiledb.object_type", return_value="array")
+@mock.patch("tiledb.cloud.array.deregister_array")
+def test_asset_deregister_array_dispatch(deregister_array, object_type):
+    """Dispatch to array.deregister_array when URI is an array."""
+    asset.deregister("a")
+    deregister_array.assert_called_once_with("a")
+
+
+@mock.patch("tiledb.object_type", return_value="group")
+@mock.patch("tiledb.cloud.groups.deregister")
+def test_asset_deregister_group_dispatch(deregister_group, object_type):
+    """Dispatch to groups.deregister when URI is a group."""
+    asset.deregister("g")
+    deregister_group.assert_called_once_with("g", recursive=False)
+
+
+@mock.patch("tiledb.object_type", return_value="group")
+@mock.patch("tiledb.cloud.groups.deregister")
+def test_asset_deregister_group_recursive_dispatch(deregister_group, object_type):
+    """Dispatch to groups.deregister when URI is a group."""
+    asset.deregister("g", recursive=True)
+    deregister_group.assert_called_once_with("g", recursive=True)
+
+
+class RegistrationTest(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.namespace, storage_path, _ = groups._default_ns_path_cred()
+        self.test_path = storage_path + "/" + testonly.random_name("groups_test")
+
+    def assert_group_exists(self, name):
+        """Asserts that a group exists, and gives it a few tries."""
+        api = client.build(rest_api.GroupsApi)
+        for _ in range(TRIES):
+            try:
+                api.get_group_contents(group_namespace=self.namespace, group_name=name)
+                return
+            except rest_api.ApiException:
+                pass  # try again
+            time.sleep(1)
+        self.fail(f"group {self.namespace}/{name} does not exist")
+
+    def assert_group_not_exists(self, name):
+        """Asserts that a group does not exist, giving it a few tries to go."""
+        api = client.build(rest_api.GroupsApi)
+        for _ in range(TRIES):
+            try:
+                api.get_group_contents(group_namespace=self.namespace, group_name=name)
+            except rest_api.ApiException as apix:
+                if apix.status in (400, 404):
+                    return
+            time.sleep(1)
+        self.fail(f"group {self.namespace}/{name} still exists")
+
+    def test_create_deregister(self):
+        # TODO: This test leaves detritus around.
+        # Once we get true delete functions, clean that up.
+        outer_name = testonly.random_name("outer")
+        outer_storage_name = testonly.random_name(outer_name)
+        outer_storage_path = f"{self.test_path}/{outer_storage_name}"
+        outer_uri = f"tiledb://{self.namespace}/{outer_name}"
+        groups.create(outer_name, storage_uri=outer_storage_path)
+        self.assert_group_exists(outer_name)
+
+        asset.deregister(outer_uri)
+        self.assert_group_not_exists(outer_name)
+
+        asset.register(outer_storage_path, "group", name=outer_name)
+        self.assert_group_exists(outer_name)
+        inner_name = testonly.random_name("inner")
+        groups.create(inner_name, parent_uri=outer_uri)
+        self.assert_group_exists(inner_name)
+        for _ in range(TRIES):
+            try:
+                asset.deregister(outer_uri, recursive=True)
+                break
+            except Exception:
+                time.sleep(2)
+        self.assert_group_not_exists(outer_name)
+        self.assert_group_not_exists(inner_name)
