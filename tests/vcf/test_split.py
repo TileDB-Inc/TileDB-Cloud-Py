113a1cca7c43fedc9786d27e01ff3667f1ce77da
Spencer Seale <spencerseale17@gmail.com>
2024-08-26 19:49:19 -0700

    Add feature: Split VCF (#616)
    * add split module
    
    * add split module
    
    * import into tiledb.cloud.vcf
    
    * add split unit tests, reorg vcf tests, clean up split docs str for quarto
    
    * type mocks
    
    * rm print statement in unit tests
    
    * more verbose, fix patching
    
    * lint

---
+++ tests/vcf/test_split.py
@@ -0,0 +1,112 @@
+"""Unit tests for tiledb.cloud.vcf.split module."""
+
+import os
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import tiledb.cloud
+from tiledb.cloud.vcf.split import ls_samples
+from tiledb.cloud.vcf.split import split_one_sample
+from tiledb.cloud.vcf.split import split_vcf
+
+# test constants
+_vcf_uri = "source_vcf.vcf.gz"
+_output_uri = "output"
+
+
+@patch("tiledb.VFS")
+@patch("gzip.GzipFile")
+def test_ls_samples(mocked_gz_file: MagicMock, mocked_vfs: MagicMock) -> None:
+    """Test tiledb.cloud.vcf.split.ls_samples"""
+
+    expected = ["SampleA", "SampleB"]
+    binary_exp = "\t".join(expected).encode()
+
+    mocked_lines = [b"##SKIPME1", b"##SKIPME2", b"#CHROM\tFORMAT\t" + binary_exp]
+
+    mocked_gz_file.return_value = mocked_lines
+
+    observed = ls_samples(vcf_uri=_vcf_uri)
+
+    assert observed == expected
+
+
+@patch("tiledb.cloud.vcf.split.process_stream")
+def test_split_one_sample(mock_process_stream: MagicMock) -> None:
+    """Test tiledb.cloud.vcf.split.split_one_sample"""
+
+    sample = "SampleA"
+    mock_process_stream.return_value = (
+        0,
+        "stream output",
+        "stream err",
+    )
+
+    observed = split_one_sample(
+        vcf_uri=_vcf_uri,
+        sample=sample,
+        output_uri=_output_uri,
+    )
+
+    assert observed == os.path.join(_output_uri, sample + ".vcf.gz")
+
+
+@patch.object(tiledb.cloud.vcf.split.DAG, "submit")
+def test_split_vcf(mock_submit_1: MagicMock) -> None:
+    """Test tiledb.cloud.vcf.split.split_vcf
+
+    Pretty basic function that just prepares the DAG.
+    Need to confirm it adds nodes based on inputs.
+    """
+
+    obs = split_vcf(
+        vcf_uri=_vcf_uri,
+        output_uri=_output_uri,
+        namespace="namespace",
+        acn="acn",
+        samples=["SampleA"],
+        compute=False,
+    )
+
+    assert isinstance(obs, tiledb.cloud.dag.DAG)
+
+    # should have submitted just one node, with just one sample passed
+    mock_submit_1.assert_called_once()
+
+    assert not obs.nodes
+
+    # call again with two samples
+    # have to init a new mock
+    # with patch.object(tiledb.cloud.dag.DAG, "submit") as mock_submit_2:
+    with patch.object(tiledb.cloud.vcf.split.DAG, "submit") as mock_submit_2:
+        obs = split_vcf(
+            vcf_uri=_vcf_uri,
+            output_uri=_output_uri,
+            namespace="namespace",
+            acn="acn",
+            samples=["SampleA", "SampleB"],
+            compute=False,
+        )
+
+        assert mock_submit_2.call_count == 2
+
+    # test without any samples for dynamic sample discovery
+    with (
+        patch.object(tiledb.cloud.vcf.split.DAG, "submit") as mock_submit_3,
+        patch.object(
+            tiledb.cloud.vcf.split.DAG, "submit_udf_stage"
+        ) as mock_submit_udf_stage,
+        patch("tiledb.cloud.vcf.split.run_dag") as mock_run_dag,
+    ):
+        # also testing with compute == True
+        obs = split_vcf(
+            vcf_uri=_vcf_uri,
+            output_uri=_output_uri,
+            namespace="namespace",
+            acn="acn",
+            compute=True,
+        )
+
+        mock_submit_3.assert_called_once()
+        mock_submit_udf_stage.assert_called_once()
+        mock_run_dag.assert_called_once()
